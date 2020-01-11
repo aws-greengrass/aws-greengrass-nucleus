c6d3379b80873ea9b2951d946f24a53b956e7681
James Gosling <jag@norquay.com>
2020-01-11 13:25:22 -0800

    Making multiple coordinated updates to the system config.  It enables code fragments like:
        kernel.context.get(UpdateSystemSafelyService.class).addUpdateAction("test", 
            ()->kernel.readMerge(Kernel.class.getResource("delta.yaml"), false));
    
    The readMerge() call reads the deltas in the file "delta.yaml", which can
    also be in JSON or TLog format.  addUpdateAction() causes it to happen when
    the system is in a safe state.

--- src/main/java/com/aws/iot/evergreen/kernel/GenericExternalService.java
+++ src/main/java/com/aws/iot/evergreen/kernel/GenericExternalService.java
@@ -133,6 +133,7 @@ public class GenericExternalService extends EvergreenService {
                 neg = !neg;
             }
             expr = context.get(EZTemplates.class).rewrite(expr).toString();
+            context.getLog().note(n.getFullName(),"skip expr",expr);
             Matcher m = skipcmd.matcher(expr);
             if (m.matches())
                 switch (m.group(1)) {
@@ -149,6 +150,7 @@ public class GenericExternalService extends EvergreenService {
             // Assume it's a shell script: test for 0 return code and nothing on stderr
             return neg ^ (status!=RunStatus.Errored);
         }
+        context.getLog().note(n.getFullName(),"no skipif");
         return false;
     }
 
