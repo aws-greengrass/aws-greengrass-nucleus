011e3cc3b7c071260e86a3f7f95bc6572790d312
Rahul Chauhan <chaurah@amazon.com>
2020-02-24 11:26:56 -0800

    Add Local Artifact Store and tests

--- src/test/java/com/aws/iot/evergreen/packagemanager/TestHelper.java
+++ src/test/java/com/aws/iot/evergreen/packagemanager/TestHelper.java
@@ -18,6 +18,37 @@ public class TestHelper {
     public static final String CONVEYOR_BELT_PACKAGE_NAME = "ConveyorBelt";
     public static final String INVALID_VERSION_PACKAGE_NAME = "InvalidRecipeVersion";
     public static final String NO_DEFAULT_CONFIG_PACKAGE_NAME = "NoDefaultPlatformConfig";
+    public static final String LOG_PACKAGE_NAME = "Log";
+    public static final String COOL_DB_PACKAGE_NAME = "Cool-Database";
+
+    public static Path getPathForLocalWorkingDirectory()
+            throws URISyntaxException, IOException {
+        Path path = Paths.get(TestHelper.class.getResource("plugins").toURI()).resolve("test_cache_working");
+        if(Files.notExists(path)) {
+            Files.createDirectories(path);
+        }
+        return path;
+    }
+
+    public static Path getPathForLocalTestCache()
+            throws URISyntaxException, IOException {
+        Path path = Paths.get(TestHelper.class.getResource("plugins").toURI()).resolve("test_cache_local");
+        if(Files.notExists(path)) {
+            Files.createDirectories(path);
+        }
+        return path;
+    }
+
+    public static Path getPathForMockRepository()
+            throws URISyntaxException {
+        return Paths.get(TestHelper.class.getResource("mock_artifact_source").toURI());
+    }
+
+    public static Path getPathForTestPackage(String testPackageName, String testPackageVersion)
+            throws URISyntaxException {
+        Path rootPath = Paths.get(TestHelper.class.getResource("test_packages").toURI());
+        return rootPath.resolve(testPackageName + "-" + testPackageVersion);
+    }
 
     static {
         OBJECT_MAPPER.configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES, true);
@@ -31,6 +62,14 @@ public class TestHelper {
             throws IOException, URISyntaxException {
         Path rootPath = Paths.get(TestHelper.class.getResource("test_packages").toURI());
         Path path = rootPath.resolve(testPackageName + "-" + testPackageVersion).resolve("recipe.yaml");
+        String recipeFmt = new String(Files.readAllBytes(path));
+        return String.format(recipeFmt, rootPath.toString());
+    }
+
+    public static String getPackageRecipeFromMockRepository(String testPackageName, String testPackageVersion)
+            throws IOException, URISyntaxException {
+        Path rootPath = Paths.get(TestHelper.class.getResource("mock_artifact_source").toURI());
+        Path path = rootPath.resolve(testPackageName).resolve(testPackageVersion).resolve("recipe.yaml");
         String recipeFmt = new String(Files.readAllBytes(path));
         return String.format(recipeFmt, rootPath.toString());
     }
