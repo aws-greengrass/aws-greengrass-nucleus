91e56776e28ecca8a4164f000f4595e16baf5f3a
Jason Wang <zhengang@amazon.com>
2020-02-24 13:41:43 -0800

    make findbug happy by uncomment private method now

--- src/main/java/com/aws/iot/evergreen/packagemanagement/PackageManager.java
+++ src/main/java/com/aws/iot/evergreen/packagemanagement/PackageManager.java
@@ -2,10 +2,10 @@ package com.aws.iot.evergreen.packagemanagement;
 
 import com.aws.iot.evergreen.packagemanagement.model.Package;
 import com.aws.iot.evergreen.packagemanagement.model.PackageMetadata;
-import com.aws.iot.evergreen.packagemanagement.model.PackageRegistryEntry;
-import com.aws.iot.evergreen.packagemanagement.model.PackageVersionConflictException;
-
-import java.util.Collections;
+//import com.aws.iot.evergreen.packagemanagement.model.PackageRegistryEntry;
+//import com.aws.iot.evergreen.packagemanagement.model.PackageVersionConflictException;
+//
+//import java.util.Collections;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.Future;
@@ -20,27 +20,27 @@ public class PackageManager {
         return null;
     }
 
-    /*
-     * Given a set of proposed package dependency trees,
-     * figure out new package dependencies.
-     */
-    private Set<PackageRegistryEntry> resolveNewPackagesDependencies(Set<PackageMetadata> proposedPackages)
-            throws PackageVersionConflictException {
-        return Collections.emptySet();
-    }
-
-    /*
-     * Given a set of pending refresh packages, download the package recipes and artifacts in background
-     * Return the packages got successfully downloaded
-     */
-    private Set<PackageRegistryEntry> downloadPackages(Set<PackageRegistryEntry> pendingDownloadPackages) {
-        return null;
-    }
-
-    /*
-     * Given a set of target packages, return their resolved dependency trees with recipe data initialized
-     */
-    private Map<PackageMetadata, Package> loadPackages(Set<PackageMetadata> proposedPackages) {
-        return Collections.emptyMap();
-    }
+//    /*
+//     * Given a set of proposed package dependency trees,
+//     * figure out new package dependencies.
+//     */
+//    private Set<PackageRegistryEntry> resolveNewPackagesDependencies(Set<PackageMetadata> proposedPackages)
+//            throws PackageVersionConflictException {
+//        return Collections.emptySet();
+//    }
+//
+//    /*
+//     * Given a set of pending refresh packages, download the package recipes and artifacts in background
+//     * Return the packages got successfully downloaded
+//     */
+//    private Set<PackageRegistryEntry> downloadPackages(Set<PackageRegistryEntry> pendingDownloadPackages) {
+//        return null;
+//    }
+//
+//    /*
+//     * Given a set of target packages, return their resolved dependency trees with recipe data initialized
+//     */
+//    private Map<PackageMetadata, Package> loadPackages(Set<PackageMetadata> proposedPackages) {
+//        return Collections.emptyMap();
+//    }
 }
