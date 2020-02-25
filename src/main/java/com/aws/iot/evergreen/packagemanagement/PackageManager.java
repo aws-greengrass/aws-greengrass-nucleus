56d16e410dffbd92d3038fb164eadfbdc7a424f0
Jason Wang <zhengang@amazon.com>
2020-02-25 11:51:36 -0800

    delete comments

--- src/main/java/com/aws/iot/evergreen/packagemanagement/PackageManager.java
+++ src/main/java/com/aws/iot/evergreen/packagemanagement/PackageManager.java
@@ -7,11 +7,6 @@ import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.Future;
 
-//import com.aws.iot.evergreen.packagemanagement.model.PackageRegistryEntry;
-//import com.aws.iot.evergreen.packagemanagement.model.PackageVersionConflictException;
-//
-//import java.util.Collections;
-
 public class PackageManager {
 
     /*
@@ -22,27 +17,4 @@ public class PackageManager {
         return null;
     }
 
-    //    /*
-    //     * Given a set of proposed package dependency trees,
-    //     * figure out new package dependencies.
-    //     */
-    //    private Set<PackageRegistryEntry> resolveNewPackagesDependencies(Set<PackageMetadata> proposedPackages)
-    //            throws PackageVersionConflictException {
-    //        return Collections.emptySet();
-    //    }
-    //
-    //    /*
-    //     * Given a set of pending refresh packages, download the package recipes and artifacts in background
-    //     * Return the packages got successfully downloaded
-    //     */
-    //    private Set<PackageRegistryEntry> downloadPackages(Set<PackageRegistryEntry> pendingDownloadPackages) {
-    //        return null;
-    //    }
-    //
-    //    /*
-    //     * Given a set of target packages, return their resolved dependency trees with recipe data initialized
-    //     */
-    //    private Map<PackageMetadata, Package> loadPackages(Set<PackageMetadata> proposedPackages) {
-    //        return Collections.emptyMap();
-    //    }
 }
