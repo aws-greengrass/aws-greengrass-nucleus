ae25c46465fb3e08c1422577468ec2ee63fd4bb7
Amit Banthiya <abanthiy@amazon.com>
2020-02-26 02:31:39 -0800

    Removing wget from github workflow. Removing the deployment service integ test from kernel tests. Addin files deleted in merge from master

---
+++ src/main/java/com/aws/iot/evergreen/packagemanager/PackageManager.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ */
+
+package com.aws.iot.evergreen.packagemanager;
+
+import com.aws.iot.evergreen.packagemanager.models.Package;
+import com.aws.iot.evergreen.packagemanager.models.PackageMetadata;
+
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.Future;
+
+public class PackageManager {
+    /*
+     * Given a set of proposed package dependency trees.
+     * Return the local resolved dependency tress in the future
+     */
+    public Future<Map<PackageMetadata, Package>> resolvePackages(Set<PackageMetadata> proposedPackages) {
+        return null;
+    }
+}
