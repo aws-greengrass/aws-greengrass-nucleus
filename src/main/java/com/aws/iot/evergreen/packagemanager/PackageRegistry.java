5f252661a139957f7b7fb8b5cdec0556f1b25277
Jason Wang <zhengang@amazon.com>
2020-02-25 19:01:12 -0800

    handle some comments

--- src/main/java/com/aws/iot/evergreen/packagemanager/PackageRegistry.java
+++ src/main/java/com/aws/iot/evergreen/packagemanager/PackageRegistry.java
@@ -6,8 +6,16 @@ import java.util.List;
 
 public interface PackageRegistry {
 
+    /**
+     * find all the active packages registered
+     * @return list of package registry entry
+     */
     List<PackageRegistryEntry> findActivePackages();
 
+    /**
+     * update the active packages registered
+     * @param list of package registry entry to be updated
+     */
     void updateActivePackages(List<PackageRegistryEntry> activePackages);
 
 }
