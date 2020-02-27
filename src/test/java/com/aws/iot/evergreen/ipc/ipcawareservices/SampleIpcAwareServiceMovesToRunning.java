ef8058b37226b47669d1453310b2e6b407079b45
fahadmohammed01 <52720275+fahadmohammed01@users.noreply.github.com>
2020-02-26 16:09:37 -0800

    integration test for ipc aware service (#77)
    * integration test for ipc aware service

---
+++ src/test/java/com/aws/iot/evergreen/ipc/ipcawareservices/SampleIpcAwareServiceMovesToRunning.java
@@ -0,0 +1,15 @@
+package com.aws.iot.evergreen.ipc.ipcawareservices;
+
+import com.aws.iot.evergreen.ipc.IPCClient;
+import com.aws.iot.evergreen.ipc.IPCClientImpl;
+import com.aws.iot.evergreen.ipc.config.KernelIPCClientConfig;
+import com.aws.iot.evergreen.ipc.services.lifecycle.LifecycleImpl;
+
+public class SampleIpcAwareServiceMovesToRunning {
+    public static void main(String[] args) throws Exception {
+        IPCClient client = new IPCClientImpl(KernelIPCClientConfig.builder().build());
+        LifecycleImpl c = new LifecycleImpl(client);
+        c.reportState("RUNNING");
+        client.disconnect();
+    }
+}
