e2fc6beb22451b055dcdfbe90fe7da49210524b6
Hui Yang <huiyn@amazon.com>
2020-03-11 12:02:51 -0700

    Update dependency resolver interface

--- src/main/java/com/aws/iot/evergreen/packagemanager/KernelConfigResolver.java
+++ src/main/java/com/aws/iot/evergreen/packagemanager/KernelConfigResolver.java
@@ -4,7 +4,6 @@ import com.aws.iot.evergreen.deployment.model.DeploymentDocument;
 import com.aws.iot.evergreen.packagemanager.models.PackageIdentifier;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class KernelConfigResolver {
@@ -20,12 +19,12 @@ public class KernelConfigResolver {
      * Create a kernel config map from a list of package identifiers and deployment document.
      * For each package, it first retrieves its recipe, then merge the parameter values into the recipe, and last
      * transform it to a kernel config key-value pair.
-     * @param pkgs a list of package identifiers
+     * @param pkgs a map of package identifiers to version constraints
      * @param document deployment document
      * @return a kernel config map
      * @throws InterruptedException when the running thread is interrupted
      */
-    public Map<Object, Object> resolve(List<PackageIdentifier> pkgs, DeploymentDocument document)
+    public Map<Object, Object> resolve(Map<PackageIdentifier, String> pkgs, DeploymentDocument document)
             throws InterruptedException {
         return new HashMap<>();
     }
