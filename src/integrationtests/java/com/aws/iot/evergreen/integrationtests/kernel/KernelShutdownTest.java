b7c3cf9a328aebde19d1c338d28985d1044faf3b
fahadmohammed01 <52720275+fahadmohammed01@users.noreply.github.com>
2020-03-12 18:26:03 -0700

    updated kernel shutdown integration test (#112)
    * updated kernel shutdown integration test
    
    * updated test to use GlobalStateChangeListener
    
    * fixed bug in finding dependers
    
    * using atomic boolean instead of countdown latch

--- src/integrationtests/java/com/aws/iot/evergreen/integrationtests/kernel/KernelShutdownTest.java
+++ src/integrationtests/java/com/aws/iot/evergreen/integrationtests/kernel/KernelShutdownTest.java
@@ -3,8 +3,6 @@ package com.aws.iot.evergreen.integrationtests.kernel;
 import com.aws.iot.evergreen.config.Topic;
 import com.aws.iot.evergreen.config.WhatHappened;
 import com.aws.iot.evergreen.dependency.State;
-import com.aws.iot.evergreen.kernel.EvergreenService;
-import com.aws.iot.evergreen.kernel.GenericExternalService;
 import com.aws.iot.evergreen.kernel.Kernel;
 import com.aws.iot.evergreen.testcommons.extensions.PerformanceReporting;
 import org.junit.jupiter.api.BeforeEach;
@@ -13,13 +11,10 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.io.TempDir;
 
 import java.nio.file.Path;
-import java.util.Collections;
-import java.util.List;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
-import java.util.stream.Collectors;
+import java.util.concurrent.atomic.AtomicBoolean;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @ExtendWith(PerformanceReporting.class)
@@ -40,6 +35,26 @@ class KernelShutdownTest {
 
     @Test
     void WHEN_kernel_shutdown_THEN_services_are_shutdown_in_reverse_dependecy_order() throws InterruptedException {
+        AtomicBoolean mainClosed = new AtomicBoolean(false);
+        AtomicBoolean sleeperAClosed = new AtomicBoolean(false);
+        AtomicBoolean sleeperBClosed = new AtomicBoolean(false);
+
+        kernel.context.addGlobalStateChangeListener((service, oldState, newState) -> {
+            if (service.getName().equals("main") && newState.isClosable()) {
+                mainClosed.set(true);
+            }
+            // Only count main as started if its dependency (new_service) has already been started
+            if (mainClosed.get()) {
+                if (service.getName().equals("sleeperA") && newState.isClosable()) {
+                    sleeperAClosed.set(true);
+                }
+            }
+            if (sleeperAClosed.get()) {
+                if (service.getName().equals("sleeperB") && newState.isClosable()) {
+                    sleeperBClosed.set(true);
+                }
+            }
+        });
 
         CountDownLatch mainRunningLatch = new CountDownLatch(1);
         kernel.getMain().getStateTopic().subscribe((WhatHappened what, Topic t) -> {
@@ -47,23 +62,10 @@ class KernelShutdownTest {
                 mainRunningLatch.countDown();
             }
         });
+
         //wait for main to run
         assertTrue(mainRunningLatch.await(60, TimeUnit.SECONDS));
         kernel.shutdown(60);
-
-        List<EvergreenService> genericExternalServices = kernel.orderedDependencies().stream()
-                .filter(e -> e instanceof GenericExternalService).collect(Collectors.toList());
-
-        assertTrue(genericExternalServices.stream().allMatch(e -> e.getState().isClosable()));
-
-        //sorting genericExternalServices in descending order based on when they reached terminal state
-        Collections.sort(genericExternalServices, (s1, s2) ->
-                Long.compare(s1.getStateTopic().getModtime(), s1.getStateTopic().getModtime()));
-
-        // service should moved to terminal state based on dependency order
-        assertEquals(3, genericExternalServices.size());
-        assertEquals("main", genericExternalServices.get(2).getName());
-        assertEquals("sleeperA", genericExternalServices.get(1).getName());
-        assertEquals("sleeperB", genericExternalServices.get(0).getName());
+        assertTrue(sleeperBClosed.get());
     }
 }
