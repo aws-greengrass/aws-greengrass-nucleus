5a60b3d2f794aa058ac7e07330dca185469465f1
Amit Banthiya <abanthiy@amazon.com>
2020-03-17 11:33:20 -0700

    Changing the integ tests to run kernel only once, before starting any tests

--- src/integrationtests/java/com/aws/iot/evergreen/integrationtests/deployment/DeploymentServiceIntegrationTest.java
+++ src/integrationtests/java/com/aws/iot/evergreen/integrationtests/deployment/DeploymentServiceIntegrationTest.java
@@ -20,9 +20,13 @@ import com.aws.iot.evergreen.packagemanager.plugins.LocalPackageStore;
 import com.aws.iot.evergreen.testcommons.extensions.PerformanceReporting;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.io.TempDir;
 
@@ -47,6 +51,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
 @ExtendWith(PerformanceReporting.class)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class DeploymentServiceIntegrationTest {
 
     private static final String TEST_CUSTOMER_APP_STRING = "Hello evergreen. This is a test";
@@ -69,9 +74,8 @@ public class DeploymentServiceIntegrationTest {
     private static KernelConfigResolver kernelConfigResolver;
 
     private DeploymentDocument sampleDeploymentDocument;
-    private Kernel kernel;
-    @TempDir
-    Path tempRootDir;
+    private static Kernel kernel;
+
     private static Map<String, Long> outputMessagesToTimestamp;
     private CountDownLatch countDownLatch;
 
@@ -83,11 +87,12 @@ public class DeploymentServiceIntegrationTest {
         logger = LogManager.getLogger(DeploymentServiceIntegrationTest.class);
     }
 
-    @BeforeEach
-    public void setupKernel() throws InterruptedException {
-
+    @BeforeAll
+    public static void setupKernel() throws InterruptedException {
+        //Cannot use @TempDir since it does not work with static variables
+        String tempRootDir = "~/deploymentIntegTest";
         kernel = new Kernel();
-        kernel.parseArgs("-r", tempRootDir.toString(), "-i",
+        kernel.parseArgs("-r", tempRootDir, "-i",
                 DeploymentServiceIntegrationTest.class.getResource("deploymentDemo.yaml").toString());
         kernel.launch();
         dependencyResolver = new DependencyResolver(new LocalPackageStore(LOCAL_CACHE_PATH), kernel.context);
@@ -95,7 +100,13 @@ public class DeploymentServiceIntegrationTest {
         kernelConfigResolver = new KernelConfigResolver(packageCache, kernel);
     }
 
+    @AfterAll
+    public static void tearDown() {
+        kernel.shutdown();
+    }
+
     @Test
+    @Order(1)
     public void GIVEN_sample_deployment_doc_WHEN_submitted_to_deployment_task_THEN_services_start_in_kernel()
             throws Exception {
         outputMessagesToTimestamp.clear();
@@ -122,7 +133,7 @@ public class DeploymentServiceIntegrationTest {
             fail("Failed executing the deployment task", e.getCause());
         }
 
-        countDownLatch.await(10, TimeUnit.SECONDS);
+        countDownLatch.await(60, TimeUnit.SECONDS);
         Set<String> listOfStdoutMessagesTapped = outputMessagesToTimestamp.keySet();
         assertTrue(listOfStdoutMessagesTapped.containsAll(listOfExpectedMessages));
         assertTrue(outputMessagesToTimestamp.get(TEST_TICK_TOCK_STRING) <
@@ -130,10 +141,10 @@ public class DeploymentServiceIntegrationTest {
         assertTrue(outputMessagesToTimestamp.get(TEST_MOSQUITTO_STRING) <
                 outputMessagesToTimestamp.get(TEST_CUSTOMER_APP_STRING));
         Log4jLogEventBuilder.removeGlobalListener(listener);
-        kernel.shutdown();
     }
 
     @Test
+    @Order(2)
     public void GIVEN_services_running_WHEN_updated_params_THEN_services_start_with_updated_params_in_kernel()
             throws Exception {
         outputMessagesToTimestamp.clear();
@@ -147,25 +158,17 @@ public class DeploymentServiceIntegrationTest {
             }
         };
         Log4jLogEventBuilder.addGlobalListener(listener);
-        Future<?> result = submitSampleJobDocument(DeploymentServiceIntegrationTest.class.getResource(
-                "SampleJobDocument.json").toURI());
-        try {
-            result.get();
-        } catch (ExecutionException e) {
-            fail("Failed executing the deployment task", e.getCause());
-        }
 
-        result = submitSampleJobDocument(DeploymentServiceIntegrationTest.class.getResource(
+        Future<?> result = submitSampleJobDocument(DeploymentServiceIntegrationTest.class.getResource(
                 "SampleJobDocument_updated.json").toURI());
         try {
             result.get();
         } catch (ExecutionException e) {
             fail("Failed executing the updated deployment task", e.getCause());
         }
-        countDownLatch.await(10, TimeUnit.SECONDS);
+        countDownLatch.await(60, TimeUnit.SECONDS);
         assertTrue(outputMessagesToTimestamp.containsKey(TEST_CUSTOMER_APP_STRING_UPDATED));
         Log4jLogEventBuilder.removeGlobalListener(listener);
-        kernel.shutdown();
     }
 
     private Future<?> submitSampleJobDocument(URI uri) {
