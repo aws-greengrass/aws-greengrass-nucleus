5f184ed1435575af4c7eeb5dbcf0245e014ec7b2
Ethan Huang <shuyeh@amazon.com>
2020-03-18 15:32:40 -0700

    Add AbstractBaseITCase to make integration test consistent with the help of reading root from System Property

--- src/integrationtests/java/com/aws/iot/evergreen/integrationtests/ipc/IPCAwareServicesTest.java
+++ src/integrationtests/java/com/aws/iot/evergreen/integrationtests/ipc/IPCAwareServicesTest.java
@@ -43,8 +43,7 @@ class IPCAwareServicesTest extends AbstractBaseITCase {
     }
 
     @Test
-    void GIVEN_ipc_aware_service_WHEN_report_state_as_running_THEN_kernel_updates_state_as_running()
-            throws Exception {
+    void GIVEN_ipc_aware_service_WHEN_report_state_as_running_THEN_kernel_updates_state_as_running() throws Exception {
         CountDownLatch serviceRunning = new CountDownLatch(1);
         EvergreenService.GlobalStateChangeListener listener = (service, oldState, newState) -> {
             if (SAMPLE_IPC_AWARE_SERVICE_NAME.equals(service.getName()) && State.RUNNING.equals(newState)) {
