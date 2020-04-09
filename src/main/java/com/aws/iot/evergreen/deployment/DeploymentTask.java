8744e30c0b1ebd116ba5a8a924370e3a79dbfee4
Jason <56933686+wikimonkey@users.noreply.github.com>
2020-04-08 18:50:46 -0700

    remove import

--- src/main/java/com/aws/iot/evergreen/deployment/DeploymentTask.java
+++ src/main/java/com/aws/iot/evergreen/deployment/DeploymentTask.java
@@ -19,7 +19,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.Callable;
-import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
 /**
