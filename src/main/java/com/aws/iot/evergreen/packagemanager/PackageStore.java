f6b64107d006490c129b8d8df9d88ec7740b4df3
Ethan Huang <shuyeh@amazon.com>
2020-04-23 10:14:51 -0700

    Implement PackageStore

---
+++ src/main/java/com/aws/iot/evergreen/packagemanager/PackageStore.java
@@ -0,0 +1,226 @@
+/* Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0 */
+
+package com.aws.iot.evergreen.packagemanager;
+
+import com.aws.iot.evergreen.logging.api.Logger;
+import com.aws.iot.evergreen.logging.impl.LogManager;
+import com.aws.iot.evergreen.packagemanager.exceptions.PackageLoadingException;
+import com.aws.iot.evergreen.packagemanager.exceptions.PackagingException;
+import com.aws.iot.evergreen.packagemanager.exceptions.UnexpectedPackagingException;
+import com.aws.iot.evergreen.packagemanager.models.PackageIdentifier;
+import com.aws.iot.evergreen.packagemanager.models.PackageMetadata;
+import com.aws.iot.evergreen.packagemanager.models.PackageRecipe;
+import com.aws.iot.evergreen.util.SerializerFactory;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.vdurmont.semver4j.Requirement;
+import com.vdurmont.semver4j.Semver;
+import com.vdurmont.semver4j.SemverException;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+public class PackageStore {
+    private static final Logger logger = LogManager.getLogger(PackageStore.class);
+    private static final String RECIPE_DIRECTORY = "recipe";
+    private static final String ARTIFACT_DIRECTORY = "artifact";
+    private static final String RECIPE_FILE_NAME_FORMAT = "%s-%s.yaml";
+
+    private static final ObjectMapper RECIPE_SERIALIZER = SerializerFactory.getRecipeSerializer();
+
+    private Path recipeDirectory;
+
+    private Path artifactDirectory;
+
+    @Inject
+    public PackageStore(@Named("packageStoreDirectory") Path packageStoreDirectory) throws PackagingException {
+        this.recipeDirectory = packageStoreDirectory.resolve(RECIPE_DIRECTORY);
+        if (!Files.exists(recipeDirectory)) {
+            try {
+                Files.createDirectories(recipeDirectory);
+            } catch (IOException e) {
+                throw new PackagingException(String.format("Failed to create recipe directory %s", recipeDirectory), e);
+            }
+        }
+        this.artifactDirectory = packageStoreDirectory.resolve(ARTIFACT_DIRECTORY);
+        if (!Files.exists(artifactDirectory)) {
+            try {
+                Files.createDirectories(artifactDirectory);
+            } catch (IOException e) {
+                throw new PackagingException(String.format("Failed to create artifact directory %s", artifactDirectory),
+                        e);
+            }
+        }
+    }
+
+    /**
+     * Creates a package recipe in the package store on the disk.
+     *
+     * @param packageRecipe package recipe to be create.
+     * @throws PackageLoadingException if fails to write the package recipe to disk.
+     */
+    void createPackageRecipe(PackageRecipe packageRecipe) throws PackageLoadingException {
+        Path recipePath = resolveRecipePath(packageRecipe.getPackageName(), packageRecipe.getVersion());
+
+        try {
+            RECIPE_SERIALIZER.writeValue(recipePath.toFile(), packageRecipe);
+        } catch (IOException e) {
+            // TODO refine exception
+            throw new PackageLoadingException(String.format("Failed to save package recipe to %s", recipePath), e);
+        }
+    }
+
+    /**
+     * Find the target package recipe from package store on the disk.
+     *
+     * @param pkgId package identifier
+     * @return Optional of package recipe; empty if not found.
+     * @throws PackageLoadingException if fails to parse the recipe file.
+     */
+    Optional<PackageRecipe> findPackageRecipe(PackageIdentifier pkgId) throws PackageLoadingException {
+        Path recipePath = resolveRecipePath(pkgId.getName(), pkgId.getVersion());
+
+        logger.atDebug().setEventType("finding-package-recipe").addKeyValue("packageRecipePath", recipePath).log();
+
+        if (!Files.exists(recipePath) || !Files.isRegularFile(recipePath)) {
+            return Optional.empty();
+        }
+
+        byte[] recipeContent;
+        try {
+            recipeContent = Files.readAllBytes(recipePath);
+        } catch (IOException e) {
+            throw new PackageLoadingException(String.format("Failed to load package recipe at %s", recipePath), e);
+        }
+
+        try {
+            return Optional.of(RECIPE_SERIALIZER.readValue(recipeContent, PackageRecipe.class));
+        } catch (IOException e) {
+            throw new PackageLoadingException(String.format("Failed to parse package recipe at %s", recipePath), e);
+        }
+    }
+
+    /**
+     * Get the package recipe from package store on the disk.
+     *
+     * @param pkgId package identifier
+     * @return retrieved package recipe.
+     * @throws PackageLoadingException if fails to find the target package recipe or fails to parse the recipe file.
+     */
+    PackageRecipe getPackageRecipe(PackageIdentifier pkgId) throws PackageLoadingException {
+        Optional<PackageRecipe> optionalPackage = findPackageRecipe(pkgId);
+
+        if (!optionalPackage.isPresent()) {
+            // TODO refine exception and logs
+            throw new PackageLoadingException(
+                    String.format("The recipe for package: '%s' doesn't exist in the local package store.", pkgId));
+        }
+
+        return optionalPackage.get();
+    }
+
+    /**
+     * Get package metadata for given package name and version.
+     *
+     * @param pkgId package id
+     * @return PackageMetadata; non-null
+     * @throws PackagingException if fails to find or parse the recipe
+     */
+    PackageMetadata getPackageMetadata(PackageIdentifier pkgId) throws PackagingException {
+        PackageRecipe retrievedPackageRecipe = getPackageRecipe(pkgId);
+
+        return new PackageMetadata(
+                new PackageIdentifier(retrievedPackageRecipe.getPackageName(), retrievedPackageRecipe.getVersion()),
+                retrievedPackageRecipe.getDependencies());
+    }
+
+    /**
+     * list PackageMetadata for available packages that satisfies the requirement.
+     *
+     * @param packageName the target package
+     * @param requirement version requirement
+     * @return a list of PackageMetadata that satisfies the requirement.
+     * @throws UnexpectedPackagingException if fails to parse version directory to Semver
+     */
+    List<PackageMetadata> listAvailablePackageMetadata(final String packageName, Requirement requirement)
+            throws PackagingException {
+        File[] recipeFiles = recipeDirectory.toFile().listFiles();
+
+        if (recipeFiles == null || recipeFiles.length == 0) {
+            return Collections.emptyList();
+        }
+
+        Arrays.sort(recipeFiles);
+
+        List<PackageMetadata> packageMetadataList = new ArrayList<>();
+
+        for (File recipeFile : recipeFiles) {
+            String recipePackageName = parsePackageNameFromFileName(recipeFile.getName());
+            // Only check the recipes for the package that we're looking for
+            if (!recipePackageName.equalsIgnoreCase(packageName)) {
+                continue;
+            }
+
+            Semver version = parseVersionFromFileName(recipeFile.getName());
+            if (requirement.isSatisfiedBy(version)) {
+                packageMetadataList.add(getPackageMetadata(new PackageIdentifier(packageName, version)));
+            }
+        }
+
+        return packageMetadataList;
+    }
+
+
+    /**
+     * Resolve the artifact directory path for a target package id.
+     *
+     * @param packageIdentifier packageIdentifier
+     * @return the artifact directory path for target package.
+     */
+    Path resolveArtifactDirectoryPath(PackageIdentifier packageIdentifier) {
+        return artifactDirectory.resolve(packageIdentifier.getName())
+                .resolve(packageIdentifier.getVersion().getValue());
+    }
+
+    private Path resolveRecipePath(String packageName, Semver packageVersion) {
+        return recipeDirectory.resolve(String.format(RECIPE_FILE_NAME_FORMAT, packageName, packageVersion.getValue()));
+    }
+
+    private static String parsePackageNameFromFileName(String filename) {
+        // TODO validate filename
+
+        // MonitoringService-1.0.0.yaml
+        String suffix = ".yaml";
+        String[] packageNameAndVersionParts = filename.split(suffix)[0].split("-");
+
+        return String.join("-", Arrays.copyOf(packageNameAndVersionParts, packageNameAndVersionParts.length - 1));
+    }
+
+    private static Semver parseVersionFromFileName(String filename) throws UnexpectedPackagingException {
+        // TODO validate filename
+
+        // MonitoringService-1.0.0.yaml
+        String suffix = ".yaml";
+        String[] packageNameAndVersionParts = filename.split(suffix)[0].split("-");
+
+        // PackageRecipe name could have '-'. Pick the last part since the version is always after the package name.
+        String versionStr = packageNameAndVersionParts[packageNameAndVersionParts.length - 1];
+
+        try {
+            return new Semver(versionStr);
+        } catch (SemverException e) {
+            throw new UnexpectedPackagingException(
+                    String.format("PackageRecipe recipe file name: '%s' is corrupted!", filename), e);
+        }
+    }
+
+}
---
+++ src/test/java/com/aws/iot/evergreen/packagemanager/PackageStoreTest.java
@@ -0,0 +1,229 @@
+package com.aws.iot.evergreen.packagemanager;
+
+import com.aws.iot.evergreen.packagemanager.exceptions.PackageLoadingException;
+import com.aws.iot.evergreen.packagemanager.exceptions.PackagingException;
+import com.aws.iot.evergreen.packagemanager.models.PackageIdentifier;
+import com.aws.iot.evergreen.packagemanager.models.PackageMetadata;
+import com.aws.iot.evergreen.packagemanager.models.PackageRecipe;
+import com.aws.iot.evergreen.testcommons.testutilities.ExceptionLogProtector;
+import com.aws.iot.evergreen.util.SerializerFactory;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.vdurmont.semver4j.Requirement;
+import com.vdurmont.semver4j.Semver;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.io.TempDir;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.collection.IsIterableWithSize.iterableWithSize;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.hamcrest.io.FileMatchers.anExistingDirectory;
+import static org.hamcrest.io.FileMatchers.anExistingFile;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@ExtendWith({ExceptionLogProtector.class})
+class PackageStoreTest {
+    private static final ObjectMapper RECIPE_SERIALIZER = SerializerFactory.getRecipeSerializer();
+
+    private static final String MONITORING_SERVICE_PKG_NAME = "MonitoringService";
+    private static final Semver MONITORING_SERVICE_PKG_VERSION = new Semver("1.0.0", Semver.SemverType.NPM);
+    private static final PackageIdentifier MONITORING_SERVICE_PKG_ID =
+            new PackageIdentifier(MONITORING_SERVICE_PKG_NAME, MONITORING_SERVICE_PKG_VERSION);
+
+
+    private static final Path RECIPE_RESOURCE_PATH = Paths.get(PackageStoreTest.class.getResource("recipe").getPath());
+
+    private PackageStore packageStore;
+
+    private Path recipeDirectory;
+
+    private Path artifactDirectory;
+
+    @TempDir
+    Path packageStoreRootPath;
+
+    @BeforeEach
+    void beforeEach() throws PackagingException {
+        packageStore = new PackageStore(packageStoreRootPath.toAbsolutePath());
+        recipeDirectory = packageStoreRootPath.resolve("recipe");
+        artifactDirectory = packageStoreRootPath.resolve("artifact");
+    }
+
+    @Test
+    void WHEN_PackageStore_is_initialized_THEN_recipe_and_artifact_folders_created() {
+        assertThat(recipeDirectory.toFile(), anExistingDirectory());
+        assertThat(artifactDirectory.toFile(), anExistingDirectory());
+    }
+
+    @Test
+    void GIVEN_a_recipe_not_exists_when_createPackageRecipe_THEN_recipe_file_created()
+            throws IOException, PackageLoadingException {
+        // GIVEN
+        String fileName = "MonitoringService-1.0.0.yaml";
+        Path sourceRecipe = RECIPE_RESOURCE_PATH.resolve(fileName);
+        PackageRecipe recipe =
+                RECIPE_SERIALIZER.readValue(new String(Files.readAllBytes(sourceRecipe)), PackageRecipe.class);
+
+
+        File expectedRecipeFile = recipeDirectory.resolve(fileName).toFile();
+        assertThat(expectedRecipeFile, not(anExistingFile()));
+
+        // WHEN
+        packageStore.createPackageRecipe(recipe);
+
+        // THEN
+        assertThat(expectedRecipeFile, anExistingFile());
+        String fileContent = new String(Files.readAllBytes(expectedRecipeFile.toPath()));
+        assertThat(fileContent, is(equalTo(RECIPE_SERIALIZER.writeValueAsString(recipe))));
+    }
+
+    @Test
+    void GIVEN_a_recipe_exists_WHEN_findPackageRecipe_THEN_return_it() throws Exception {
+        // GIVEN
+        String fileName = "MonitoringService-1.0.0.yaml";
+        preloadRecipeFileFromTestResource(fileName);
+
+        Path sourceRecipe = RECIPE_RESOURCE_PATH.resolve(fileName);
+
+        // WHEN
+        Optional<PackageRecipe> optionalPackageRecipe = packageStore.findPackageRecipe(MONITORING_SERVICE_PKG_ID);
+
+        // THEN
+        assertTrue(optionalPackageRecipe.isPresent());
+
+        PackageRecipe expectedRecipe =
+                RECIPE_SERIALIZER.readValue(new String(Files.readAllBytes(sourceRecipe)), PackageRecipe.class);
+        assertThat(optionalPackageRecipe.get(), equalTo(expectedRecipe));
+    }
+
+    @Test
+    void GIVEN_a_recipe_does_not_exist_WHEN_findPackageRecipe_THEN_empty_is_returned() throws Exception {
+        // WHEN
+        Optional<PackageRecipe> optionalPackageRecipe = packageStore.findPackageRecipe(MONITORING_SERVICE_PKG_ID);
+
+        // THEN
+        assertFalse(optionalPackageRecipe.isPresent());
+    }
+
+    @Test
+    void GIVEN_an_invalid_recipe_exists_WHEN_findPackageRecipe_THEN_loading_exception_is_thrown() throws Exception {
+        // GIVEN
+        String fileName = "InvalidRecipe-1.0.0.yaml";
+        preloadRecipeFileFromTestResource(fileName);
+
+        // WHEN
+        // THEN
+        assertThrows(PackageLoadingException.class,
+                () -> packageStore.findPackageRecipe(new PackageIdentifier("InvalidRecipe", new Semver("1.0.0"))));
+    }
+
+    @Test
+    void GIVEN_a_recipe_exists_WHEN_getPackageRecipe_THEN_return_it() throws Exception {
+        // GIVEN
+        String fileName = "MonitoringService-1.0.0.yaml";
+        preloadRecipeFileFromTestResource(fileName);
+
+        // WHEN
+        PackageRecipe packageRecipe = packageStore.getPackageRecipe(MONITORING_SERVICE_PKG_ID);
+
+        // THEN
+        Path sourceRecipe = RECIPE_RESOURCE_PATH.resolve(fileName);
+
+        PackageRecipe expectedRecipe =
+                RECIPE_SERIALIZER.readValue(new String(Files.readAllBytes(sourceRecipe)), PackageRecipe.class);
+        assertThat(packageRecipe, equalTo(expectedRecipe));
+    }
+
+    @Test
+    void GIVEN_a_recipe_does_not_exist_WHEN_getPackageRecipe_THEN_loading_exception_is_thrown() throws Exception {
+        assertThrows(PackageLoadingException.class, () -> packageStore.getPackageRecipe(MONITORING_SERVICE_PKG_ID));
+    }
+
+    @Test
+    void getPackageMetadata() throws PackagingException, IOException {
+        // GIVEN
+        String fileName = "MonitoringService-1.0.0.yaml";
+
+        preloadRecipeFileFromTestResource(fileName);
+
+        // WHEN
+        PackageMetadata packageMetadata = packageStore.getPackageMetadata(MONITORING_SERVICE_PKG_ID);
+
+        // THEN
+        assertThat(packageMetadata.getPackageIdentifier(), is(MONITORING_SERVICE_PKG_ID));
+        assertThat(packageMetadata.getDependencies(), is(getExpectedDependencies(MONITORING_SERVICE_PKG_VERSION)));
+    }
+
+    @Test
+    void GIVEN_pre_loaded_package_versions_WHEN_listAvailablePackageMetadata_THEN_return_satisfiedVersion()
+            throws IOException, PackagingException {
+        // GIVEN
+        preloadRecipeFileFromTestResource("MonitoringService-1.0.0.yaml");
+        preloadRecipeFileFromTestResource("MonitoringService-1.1.0.yaml");
+        preloadRecipeFileFromTestResource("MonitoringService-2.0.0.yaml");
+        preloadRecipeFileFromTestResource("MonitoringService-3.0.0.yaml");
+        preloadRecipeFileFromTestResource("Log-1.0.0.yaml");
+
+        // WHEN
+        Requirement requirement = Requirement.buildNPM(">=1.0.0 <2.0.0");
+        List<PackageMetadata> packageMetadataList =
+                packageStore.listAvailablePackageMetadata(MONITORING_SERVICE_PKG_NAME, requirement);
+
+        // THEN
+        // expected return: MonitoringService 1.0.0 and 1.1.0
+        assertThat(packageMetadataList, iterableWithSize(2));
+
+        // 1.0.0
+        PackageMetadata packageMetadata = packageMetadataList.get(0);
+        assertThat(packageMetadata.getPackageIdentifier().getName(), is(MONITORING_SERVICE_PKG_NAME));
+        assertThat(packageMetadata.getPackageIdentifier().getVersion(), is(new Semver("1.0.0")));
+        assertThat(packageMetadata.getDependencies(), is(getExpectedDependencies(new Semver("1.0.0"))));
+
+        // 1.1.0
+        packageMetadata = packageMetadataList.get(1);
+        assertThat(packageMetadata.getPackageIdentifier().getName(), is(MONITORING_SERVICE_PKG_NAME));
+        assertThat(packageMetadata.getPackageIdentifier().getVersion(), is(new Semver("1.1.0")));
+        assertThat(packageMetadata.getDependencies(), is(getExpectedDependencies(new Semver("1.1.0"))));
+    }
+
+    private void preloadRecipeFileFromTestResource(String fileName) throws IOException {
+        Path sourceRecipe = RECIPE_RESOURCE_PATH.resolve(fileName);
+
+        Path destinationRecipe = recipeDirectory.resolve(fileName);
+
+        Files.copy(sourceRecipe, destinationRecipe);
+    }
+
+    @Test
+    void resolveArtifactDirectoryPath() {
+        Path artifactPath = packageStore.resolveArtifactDirectoryPath(MONITORING_SERVICE_PKG_ID);
+
+        Path expectedArtifactPath = artifactDirectory.resolve(MONITORING_SERVICE_PKG_ID.getName())
+                .resolve(MONITORING_SERVICE_PKG_ID.getVersion().getValue());
+        assertThat(artifactPath.toAbsolutePath(), is(equalTo(expectedArtifactPath)));
+
+    }
+
+    private static Map<String, String> getExpectedDependencies(Semver version) {
+        return new HashMap<String, String>() {{
+            put("Log", version.toString());
+            put("Cool-Database", version.toString());
+        }};
+    }
+
+}
