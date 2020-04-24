eef33d597548771131ffea4167e7faf113098168
Michael Dombrowski <mikepi3.14@gmail.com>
2020-04-24 15:26:17 -0700

    Fix race condition between reported state and actual state (#214)

--- src/main/java/com/aws/iot/evergreen/kernel/EvergreenService.java
+++ src/main/java/com/aws/iot/evergreen/kernel/EvergreenService.java
@@ -103,6 +103,20 @@ public class EvergreenService implements InjectionActions {
         return (State) state.getOnce();
     }
 
+    /**
+     * Returns true if either the current or the very last reported state (if any)
+     * is equal to the provided state.
+     *
+     * @param state state to check against
+     */
+    public boolean currentOrReportedStateIs(State state) {
+        if (state.equals(getState())) {
+            return true;
+        }
+        Optional<State> reportedState = lifecycle.lastReportedState();
+        return reportedState.isPresent() && reportedState.get().equals(state);
+    }
+
     public long getStateModTime() {
         return state.getModtime();
     }
