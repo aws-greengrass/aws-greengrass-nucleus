65f3ba587ffbe142225ef9d008763489d7fa4463
Francis Fu <fufranci@amazon.com>
2020-04-28 11:55:35 -0700

    Add negative cases

--- src/test/java/com/aws/iot/evergreen/kernel/ShellRunnerTest.java
+++ src/test/java/com/aws/iot/evergreen/kernel/ShellRunnerTest.java
@@ -17,6 +17,7 @@ import java.util.function.IntConsumer;
 
 import static com.aws.iot.evergreen.ipc.AuthHandler.SERVICE_UNIQUE_ID_KEY;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.when;
 
@@ -67,6 +68,39 @@ class ShellRunnerTest extends EGServiceTestUtil {
                 assertTrue(ok);
                 assertTrue(latch.await(2, TimeUnit.SECONDS));
                 assertEquals(0, exitCode.get());
+            }
+        }
+    }
+
+    @Test
+    void GIVEN_shell_command_that_doesnt_exist_WHEN_run_in_foreground_THEN_fails() throws Exception {
+        try (Context context = new Context()) {
+            context.put(Kernel.class, kernel);
+            final ShellRunner shellRunner = context.get(ShellRunner.class);
+            try (Exec exec = shellRunner.setup("note", "there_is_no_such_program", evergreenService)) {
+                boolean ok = shellRunner.successful(exec, "note", null, evergreenService);
+                assertFalse(ok);
+            }
+        }
+    }
+
+    @Test
+    void GIVEN_shell_command_that_doesnt_exist_WHEN_run_in_background_THEN_fails() throws Exception {
+        final AtomicInteger exitCode = new AtomicInteger();
+        final CountDownLatch latch = new CountDownLatch(1);
+        IntConsumer background = (value) -> {
+            exitCode.set(value);
+            latch.countDown();
+        };
+
+        try (Context context = new Context()) {
+            context.put(Kernel.class, kernel);
+            final ShellRunner shellRunner = context.get(ShellRunner.class);
+            try (Exec exec = shellRunner.setup("note", "there_is_no_such_program", evergreenService)) {
+                boolean ok = shellRunner.successful(exec, "note", background, evergreenService);
+                assertTrue(ok); // when runs in background, always return true
+                assertTrue(latch.await(2, TimeUnit.SECONDS));
+                assertEquals(127, exitCode.get());
             }
         }
     }
