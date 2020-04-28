1538fb961125041e04402a8fa2f874c409622e02
Rahul Singh Chauhan <chaurah@amazon.com>
2020-04-28 13:28:39 -0400

    Add integration with Package Management Service (#191)

--- src/main/java/com/aws/iot/evergreen/packagemanager/GreengrassPackageServiceHelper.java
+++ src/main/java/com/aws/iot/evergreen/packagemanager/GreengrassPackageServiceHelper.java
@@ -1,15 +1,73 @@
 package com.aws.iot.evergreen.packagemanager;
 
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.services.greengrasspackagemanagement.AWSGreengrassPackageManagement;
+import com.amazonaws.services.greengrasspackagemanagement.model.GetPackageRequest;
+import com.amazonaws.services.greengrasspackagemanagement.model.GetPackageResult;
+import com.amazonaws.services.greengrasspackagemanagement.model.RecipeFormatType;
+import com.aws.iot.evergreen.logging.api.Logger;
+import com.aws.iot.evergreen.logging.impl.LogManager;
 import com.aws.iot.evergreen.packagemanager.exceptions.PackageDownloadException;
+import com.aws.iot.evergreen.packagemanager.exceptions.PackageLoadingException;
 import com.aws.iot.evergreen.packagemanager.models.PackageIdentifier;
 import com.aws.iot.evergreen.packagemanager.models.PackageRecipe;
+import com.aws.iot.evergreen.util.SerializerFactory;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.util.ByteBufferBackedInputStream;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import javax.inject.Inject;
 
 public class GreengrassPackageServiceHelper {
 
-    //TODO connect to cloud service
-    @SuppressWarnings({"PMD.UnusedLocalVariable"})
-    PackageRecipe downloadPackageRecipe(PackageIdentifier packageIdentifier) throws PackageDownloadException {
-        // TODO: to be implemented.
-        return null;
+    private static String PACKAGE_RECIPE_PARSING_EXCEPTION_FMT = "Error parsing downloaded recipe for package %s";
+    private static String PACKAGE_RECIPE_DOWNLOAD_EXCEPTION_FMT = "Error downloading recipe for package %s";
+
+    private static final ObjectMapper RECIPE_SERIALIZER = SerializerFactory.getRecipeSerializer();
+
+    // Service logger instance
+    protected final Logger logger = LogManager.getLogger(GreengrassPackageServiceHelper.class);
+
+    private final AWSGreengrassPackageManagement evgPmsClient;
+
+    @Inject
+    public GreengrassPackageServiceHelper(GreengrassPackageServiceClientFactory clientFactory) {
+        this.evgPmsClient = clientFactory.getPmsClient();
+    }
+
+    PackageRecipe downloadPackageRecipe(PackageIdentifier packageIdentifier)
+            throws PackageDownloadException, PackageLoadingException {
+        GetPackageRequest getPackageRequest =
+                new GetPackageRequest().withPackageARN(packageIdentifier.getArn())
+                                       .withType(RecipeFormatType.YAML);
+
+        GetPackageResult getPackageResult;
+        try {
+            getPackageResult = evgPmsClient.getPackage(getPackageRequest);
+        } catch (AmazonClientException e) {
+            // TODO: This should be expanded to handle various types of retryable/non-retryable exceptions
+            String errorMsg = String.format(PACKAGE_RECIPE_DOWNLOAD_EXCEPTION_FMT,
+                                            packageIdentifier.getArn());
+            logger.atError("download-package-from-greengrass-repo", e)
+                  .addKeyValue("packageIdentifier", packageIdentifier)
+                  .addKeyValue("errorMessage", errorMsg)
+                  .log();
+            throw new PackageDownloadException(errorMsg, e);
+        }
+
+        try {
+            ByteBuffer recipeBuf = getPackageResult.getRecipe();
+            return RECIPE_SERIALIZER.readValue(new ByteBufferBackedInputStream(recipeBuf),
+                                               PackageRecipe.class);
+        } catch (IOException e) {
+            String errorMsg = String.format(PACKAGE_RECIPE_PARSING_EXCEPTION_FMT,
+                                            packageIdentifier.getArn());
+            logger.atError("download-package-from-greengrass-repo", e)
+                  .addKeyValue("packageIdentifier", packageIdentifier)
+                  .addKeyValue("errorMessage", errorMsg)
+                  .log();
+            throw new PackageLoadingException(errorMsg, e);
+        }
     }
 }
--- src/test/java/com/aws/iot/evergreen/packagemanager/plugins/GreengrassRepositoryDownloaderTest.java
+++ src/test/java/com/aws/iot/evergreen/packagemanager/plugins/GreengrassRepositoryDownloaderTest.java
@@ -1,13 +1,17 @@
 package com.aws.iot.evergreen.packagemanager.plugins;
 
+import com.aws.iot.evergreen.packagemanager.GreengrassPackageServiceClientFactory;
 import com.aws.iot.evergreen.packagemanager.TestHelper;
 import com.aws.iot.evergreen.packagemanager.models.PackageIdentifier;
 import com.aws.iot.evergreen.testcommons.testutilities.EGExtension;
 import com.vdurmont.semver4j.Semver;
+import org.apache.http.HttpStatus;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.Mock;
-import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.io.IOException;
@@ -17,28 +21,54 @@ import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.services.greengrasspackagemanagement.AWSGreengrassPackageManagement;
+import com.amazonaws.services.greengrasspackagemanagement.model.GetArtifactRequest;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.when;
 
 @ExtendWith({MockitoExtension.class, EGExtension.class})
 class GreengrassRepositoryDownloaderTest {
 
-    @Spy
-    private GreengrassRepositoryDownloader downloader;
-
     @Mock
     private HttpURLConnection connection;
 
+    @Mock
+    private AWSGreengrassPackageManagement client;
+
+    @Mock
+    private GreengrassPackageServiceClientFactory clientFactory;
+
+    private GreengrassRepositoryDownloader downloader;
+
+    @Captor
+    ArgumentCaptor<GetArtifactRequest> getArtifactRequestArgumentCaptor;
+
+    @BeforeEach
+    void beforeEach() {
+        when(clientFactory.getPmsClient()).thenReturn(client);
+        this.downloader = spy(new GreengrassRepositoryDownloader(clientFactory));
+    }
+
     @Test
     void GIVEN_artifact_url_WHEN_attempt_download_THEN_task_succeed() throws Exception {
-        doReturn("https://www.amazon.com/artifact.txt").when(downloader)
-                .getArtifactDownloadURL(anyString(), anyString());
+        AmazonServiceException ase = new AmazonServiceException("Redirect");
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Location", "https://www.amazon.com/artifact.txt");
+        ase.setStatusCode(HttpStatus.SC_MOVED_TEMPORARILY);
+        ase.setHttpHeaders(headers);
+        when(client.getArtifact(getArtifactRequestArgumentCaptor.capture())).thenThrow(ase);
+
         doReturn(connection).when(downloader).connect(any());
         when(connection.getResponseCode()).thenReturn(HttpURLConnection.HTTP_OK);
         Path mockArtifactPath = TestHelper.getPathForTestPackage(TestHelper.MONITORING_SERVICE_PACKAGE_NAME, "1.0.0")
@@ -49,7 +79,11 @@ class GreengrassRepositoryDownloaderTest {
         Path testCache = TestHelper.getPathForLocalTestCache();
         Path saveToPath = testCache.resolve("CoolService").resolve("1.0.0");
         Files.createDirectories(saveToPath);
-        downloader.downloadToPath(pkgId, new URI("greengrass:binary"), saveToPath);
+        downloader.downloadToPath(pkgId, new URI("greengrass:artifactName"), saveToPath);
+
+        GetArtifactRequest generatedRequest = getArtifactRequestArgumentCaptor.getValue();
+        assertEquals("CoolServiceARN", generatedRequest.getPackageARN());
+        assertEquals("artifactName", generatedRequest.getArtifactName());
 
         byte[] originalFile = Files.readAllBytes(mockArtifactPath);
         byte[] downloadFile = Files.readAllBytes(saveToPath.resolve("artifact.txt"));
@@ -60,8 +94,13 @@ class GreengrassRepositoryDownloaderTest {
 
     @Test
     void GIVEN_http_connection_error_WHEN_attempt_download_THEN_return_exception() throws Exception {
-        doReturn("https://www.amazon.com/artifact.txt").when(downloader)
-                .getArtifactDownloadURL(anyString(), anyString());
+        AmazonServiceException ase = new AmazonServiceException("Redirect");
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Location", "https://www.amazon.com/artifact.txt");
+        ase.setStatusCode(HttpStatus.SC_MOVED_TEMPORARILY);
+        ase.setHttpHeaders(headers);
+        when(client.getArtifact(any())).thenThrow(ase);
+
         doReturn(connection).when(downloader).connect(any());
         when(connection.getResponseCode()).thenThrow(IOException.class);
 
