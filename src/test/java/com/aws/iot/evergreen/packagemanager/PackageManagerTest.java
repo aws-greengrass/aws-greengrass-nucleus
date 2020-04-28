3710394df716257f99414c344a2e35213bef56bc
Ethan Huang <shuyeh@amazon.com>
2020-04-28 11:18:23 -0700

    Fix unused import

--- src/test/java/com/aws/iot/evergreen/packagemanager/PackageManagerTest.java
+++ src/test/java/com/aws/iot/evergreen/packagemanager/PackageManagerTest.java
@@ -40,7 +40,6 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
