63d0978c5ae45148deecdf339eb085cf64002449
Shagupta <58999292+shaguptashaikh@users.noreply.github.com>
2020-05-07 10:26:20 -0700

    Dynamic config changes in deployments (#223)

--- src/main/java/com/aws/iot/evergreen/packagemanager/models/PackageRecipe.java
+++ src/main/java/com/aws/iot/evergreen/packagemanager/models/PackageRecipe.java
@@ -6,7 +6,6 @@ package com.aws.iot.evergreen.packagemanager.models;
 import com.aws.iot.evergreen.config.PlatformResolver;
 import com.aws.iot.evergreen.util.SerializerFactory;
 import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.DeserializationContext;
@@ -22,7 +21,6 @@ import lombok.NoArgsConstructor;
 
 import java.io.IOException;
 import java.util.Collections;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -54,13 +52,9 @@ public class PackageRecipe {
 
     private final Map<String, String> dependencies;
 
-    // TODO: Needs discussion, this should probably be removed after integration demo
-    private final List<String> requires;
+    private final Map<String, Object> customConfig;
 
-    // TODO: clean up this field
-    @Deprecated
-    @JsonIgnore
-    private Set<PackageRecipe> dependencyPackageRecipes;
+    private final Map<String, String> environmentVariables;
 
     /**
      * Constructor for Jackson to deserialize.
@@ -74,7 +68,8 @@ public class PackageRecipe {
      * @param lifecycle             Lifecycle definitions
      * @param artifacts             Artifact definitions
      * @param dependencies          List of dependencies
-     * @param requires              Package Requires
+     * @param customConfig          Custom config to store in kernel config store
+     * @param environmentVariables  Environment variables for the service
      * @throws SemverException if the semver fails to be created
      */
     @JsonCreator
@@ -88,7 +83,10 @@ public class PackageRecipe {
                          @JsonProperty("Artifacts") List<String> artifacts,
                          @JsonProperty("Dependencies") @JsonDeserialize(
                                  using = MapFieldDeserializer.class) Map<String, String> dependencies,
-                         @JsonProperty("Requires") List<String> requires) {
+                         @JsonProperty("CustomConfig") @JsonDeserialize(
+                                 using = MapFieldDeserializer.class) Map<String, Object> customConfig,
+                         @JsonProperty("EnvironmentVariables") @JsonDeserialize(
+                                 using = MapFieldDeserializer.class) Map<String, String> environmentVariables) {
         this.recipeTemplateVersion = recipeTemplateVersion;
         this.packageName = packageName;
         //TODO: Figure out how to do this in deserialize (only option so far seems to be custom deserializer)
@@ -100,8 +98,8 @@ public class PackageRecipe {
         this.lifecycle = lifecycle == null ? Collections.emptyMap() : lifecycle;
         this.artifacts = artifacts == null ? Collections.emptyList() : artifacts;
         this.dependencies = dependencies == null ? Collections.emptyMap() : dependencies;
-        this.requires = requires == null ? Collections.emptyList() : requires;
-        this.dependencyPackageRecipes = new HashSet<>();
+        this.customConfig = customConfig == null ? Collections.emptyMap() : customConfig;
+        this.environmentVariables = environmentVariables == null ? Collections.emptyMap() : environmentVariables;
     }
 
     @JsonSerialize(using = SemverSerializer.class)
