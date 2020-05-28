70bbce2f6e8bed31b0916d7bdb518777b6c800a7
Mohammed <faham@amazon.com>
2020-05-27 17:08:41 -0700

    list components

--- src/integrationtests/java/com/aws/iot/evergreen/integrationtests/deployment/LocalDeploymentListenerTest.java
+++ src/integrationtests/java/com/aws/iot/evergreen/integrationtests/deployment/LocalDeploymentListenerTest.java
@@ -4,6 +4,8 @@ import com.aws.iot.evergreen.dependency.State;
 import com.aws.iot.evergreen.deployment.LocalDeploymentListener;
 import com.aws.iot.evergreen.integrationtests.e2e.util.FileUtils;
 import com.aws.iot.evergreen.kernel.Kernel;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -15,10 +17,14 @@ import java.nio.file.Paths;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class LocalDeploymentListenerTest {
 
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper()
+            .configure(DeserializationFeature.FAIL_ON_INVALID_SUBTYPE, false)
+            .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
     private static Kernel kernel;
     private static LocalDeploymentListener localDeploymentListener;
 
@@ -42,7 +48,7 @@ public class LocalDeploymentListenerTest {
     @Test
     void GIVEN_sample_deployment_doc_WHEN_submitted_to_deployment_task_THEN_services_start_in_kernel() throws Exception {
         String deploymentDocument = new String(Files.readAllBytes(
-                Paths.get(DeploymentTaskIntegrationTest.class.getResource("SampleJobDocument.json").toURI())));
+                Paths.get(DeploymentTaskIntegrationTest.class.getResource("SampleJobDocument_updated.json").toURI())));
         assertTrue(localDeploymentListener.submitLocalDeployment(deploymentDocument));
 
         CountDownLatch customerAppRunningLatch = new CountDownLatch(1);
@@ -52,6 +58,16 @@ public class LocalDeploymentListenerTest {
             }
         });
         assertTrue(customerAppRunningLatch.await(50, TimeUnit.SECONDS));
+
+        LocalDeploymentListener.ListComponentsResult listComponentsResult = OBJECT_MAPPER.readValue(
+                localDeploymentListener.listComponents(), LocalDeploymentListener.ListComponentsResult.class);
+
+        assertEquals(1, listComponentsResult.getRootPackages().size());
+        assertEquals(3, listComponentsResult.getComponentsInfo().size());
+        LocalDeploymentListener.ComponentInfo customerApp = listComponentsResult.getComponentsInfo().get(2);
+        assertEquals("CustomerApp", customerApp.getPackageName());
+        assertEquals("1.0.0", customerApp.getVersion());
+        assertEquals("This is a new value", customerApp.getRuntimeParameters().get("sampleText"));
     }
 
 }
--- src/main/java/com/aws/iot/evergreen/deployment/LocalDeploymentListener.java
+++ src/main/java/com/aws/iot/evergreen/deployment/LocalDeploymentListener.java
@@ -1,26 +1,51 @@
 package com.aws.iot.evergreen.deployment;
 
+import com.aws.iot.evergreen.config.Topic;
+import com.aws.iot.evergreen.config.Topics;
+import com.aws.iot.evergreen.deployment.exceptions.DeviceConfigurationException;
 import com.aws.iot.evergreen.deployment.model.Deployment;
+import com.aws.iot.evergreen.kernel.EvergreenService;
+import com.aws.iot.evergreen.kernel.GenericExternalService;
+import com.aws.iot.evergreen.kernel.Kernel;
 import com.aws.iot.evergreen.logging.api.Logger;
 import com.aws.iot.evergreen.logging.impl.LogManager;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.vdurmont.semver4j.Semver;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.stream.Collectors;
 import javax.inject.Inject;
 import javax.inject.Named;
 
 import static com.aws.iot.evergreen.deployment.model.Deployment.DeploymentType;
+import static com.aws.iot.evergreen.packagemanager.KernelConfigResolver.PARAMETERS_CONFIG_KEY;
+import static com.aws.iot.evergreen.packagemanager.KernelConfigResolver.VERSION_CONFIG_KEY;
 
 @NoArgsConstructor
 public class LocalDeploymentListener {
 
     public static final String LOCAL_DEPLOYMENT_ID_PREFIX = "Local-";
     private static final String DEPLOYMENT_ID_LOG_KEY_NAME = "DeploymentId";
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper()
+            .configure(DeserializationFeature.FAIL_ON_INVALID_SUBTYPE, false)
+            .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
     private static Logger logger = LogManager.getLogger(LocalDeploymentListener.class);
 
     @Inject
     @Named("deploymentsQueue")
     private LinkedBlockingQueue<Deployment> deploymentsQueue;
+
+    @Inject
+    private Kernel kernel;
 
     //TODO: LocalDeploymentListener should register with IPC to expose submitLocalDeployment
     //TODO: the interface is not finalized yet, this is more an example.
@@ -37,6 +62,77 @@ public class LocalDeploymentListener {
             return true;
         }
         return false;
+    }
+
+    /**
+     * Retrieves root level components names, component information and runtime parameters.
+     * @return serialized ListComponentsResult
+     * @throws DeviceConfigurationException failure to serialize ListComponentsResult
+     */
+    public String listComponents() throws DeviceConfigurationException {
+        try {
+            List<String> rootComponenetNames = kernel.getMain().getDependencies().keySet().stream()
+                    .filter(evergreenService -> evergreenService instanceof GenericExternalService)
+                    .map(EvergreenService::getName).collect(Collectors.toList());
+
+            List<ComponentInfo> componentInfo = kernel.orderedDependencies().stream()
+                    .filter(service -> service instanceof GenericExternalService)
+                    .filter(service -> !service.getName().equals(kernel.getMain().getName()))
+                    .map(service -> {
+                        Topic version = service.getConfig().find(VERSION_CONFIG_KEY);
+                        Topics parameters = service.getConfig().findTopics(PARAMETERS_CONFIG_KEY);
+                        ComponentInfo.ComponentInfoBuilder componentInfoBuilder = ComponentInfo.builder()
+                                .packageName(service.getName());
+                        if (version != null) {
+                            componentInfoBuilder.version(((Semver) version.getOnce()).getValue());
+                        }
+                        if (parameters != null) {
+                            componentInfoBuilder.runtimeParameters(parameters.children.entrySet().stream().collect(
+                                    Collectors.toMap(e -> e.getKey(), e -> (String) ((Topic) e.getValue()).getOnce())));
+
+                        }
+                        return componentInfoBuilder.build();
+                    }).collect(Collectors.toList());
+
+            return OBJECT_MAPPER.writeValueAsString(new ListComponentsResult(rootComponenetNames, componentInfo));
+        } catch (JsonProcessingException e) {
+            //TODO: during IPC integration, change this to report internal error
+            throw new DeviceConfigurationException("Unable to list components", e);
+        }
+    }
+
+    //TODO: move this data object to appropriate place during IPC integration.
+    /**
+     * Data object used to transfer currently running.
+     */
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Getter
+    public static class ListComponentsResult {
+        @JsonProperty("RootPackages")
+        private List<String> rootPackages;
+
+        @JsonProperty("Components")
+        private List<ComponentInfo> componentsInfo;
+
+    }
+
+    //TODO: move this data object to appropriate place during IPC integration.
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Getter
+    public static class ComponentInfo {
+        @JsonProperty("Name")
+        private String packageName;
+
+        // TODO: change to versionRequirements which can be a pinned version or a version range
+        @JsonProperty("Version")
+        private String version;
+
+        @JsonProperty("RuntimeParameters")
+        private Map<String, String> runtimeParameters;
+
     }
 
 }
