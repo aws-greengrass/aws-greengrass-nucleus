08fbc00a04aa8cb4d12272eb8d42dd91929e5d36
ShirleyZheng92 <ShirleyZ92@outlook.com>
2020-06-04 17:20:40 -0700

    Add more output in launch evergreen

--- m1-demo/pi_demo_code/evergreen-install/launch-evergreen.sh
+++ m1-demo/pi_demo_code/evergreen-install/launch-evergreen.sh
@@ -2,8 +2,14 @@
 
 echo "Starting Evergreen Kernel..."
 
-java -Dlog.store=FILE -Dlog.storeName=/home/pi/.evergreen/evergreen.log -Droot=~/.evergreen -cp EvergreenWithHttpServer.jar com.aws.iot.evergreen.easysetup.EvergreenSetup -p true -ar us-east-1 -t true -tra abanthiyTestRoleAlias -i onlyMain.yaml -tn EthanTestPi_1 > /dev/null 2>&1 &
+HOME_DIR=/home/pi/.evergreen
+THING_NAME=EthanTestPi_1
+java -Dlog.store=FILE -Dlog.storeName=${HOME_DIR}/evergreen.log -Droot=${HOME_DIR} -cp EvergreenWithHttpServer.jar com.aws.iot.evergreen.easysetup.EvergreenSetup -p true -ar us-east-1 -t true -tra abanthiyTestRoleAlias -i onlyMain.yaml -tn ${THING_NAME} > /dev/null 2>&1 &
 
 sleep 20
 
-echo "Evergreen Kernel has started successfully. Please go to localhost:1441 to view the local dashboard".
+echo "Evergreen Kernel has started successfully."
+echo "root dir: "${HOME_DIR}
+echo "log path: "${HOME_DIR}/evergreen.log
+echo "thing name: "${THING_NAME}
+echo "Please go to localhost:1441 to view the local dashboard".
