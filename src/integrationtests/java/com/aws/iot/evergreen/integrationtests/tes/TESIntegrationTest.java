c5073e0e3d62f0e81a24ceb22ff6b54c1afe4e7f
Zhaoyan Lin <zhaoylin@amazon.com>
2020-07-07 12:25:33 -0700

    Add tes happy case test

--- src/integrationtests/java/com/aws/iot/evergreen/integrationtests/tes/TESIntegrationTest.java
+++ src/integrationtests/java/com/aws/iot/evergreen/integrationtests/tes/TESIntegrationTest.java
@@ -2,24 +2,22 @@ package com.aws.iot.evergreen.integrationtests.tes;
 
 import com.aws.iot.evergreen.config.Topics;
 import com.aws.iot.evergreen.easysetup.DeviceProvisioningHelper;
-import com.aws.iot.evergreen.integrationtests.e2e.util.NetworkUtils;
 import com.aws.iot.evergreen.kernel.Kernel;
 import com.aws.iot.evergreen.testcommons.testutilities.EGExtension;
+import com.aws.iot.evergreen.util.Exec;
 import com.aws.iot.evergreen.util.IotSdkClientFactory;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
-//import java.io.PrintStream;
 
 import static com.aws.iot.evergreen.easysetup.DeviceProvisioningHelper.ThingInfo;
 import static com.aws.iot.evergreen.kernel.EvergreenService.SERVICES_NAMESPACE_TOPIC;
 import static com.aws.iot.evergreen.kernel.EvergreenService.SETENV_CONFIG_NAMESPACE;
 import static com.aws.iot.evergreen.tes.TokenExchangeService.IOT_ROLE_ALIAS_TOPIC;
 import static com.aws.iot.evergreen.tes.TokenExchangeService.TOKEN_EXCHANGE_SERVICE_TOPICS;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.matchesPattern;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @ExtendWith(EGExtension.class)
@@ -39,8 +37,6 @@ class TESIntegrationTest {
     void setupKernel() throws IOException {
         kernel = new Kernel();
         kernel.parseArgs("-i", TESIntegrationTest.class.getResource("tesExample.yaml").toString());
-
-//        this.deviceProvisioningHelper = new DeviceProvisioningHelper("us-west-2", new PrintStream("tt"));
         this.deviceProvisioningHelper = new DeviceProvisioningHelper(AWS_REGION, System.out);
         provision(kernel);
     }
@@ -53,21 +49,14 @@ class TESIntegrationTest {
     }
 
     @Test
-    void GIVEN_iot_role_alias_WHEN_tes_is_queried_THEN_correct_credentials_are_returned() throws Exception {
+    void GIVEN_iot_role_alias_WHEN_tes_is_queried_THEN_valid_credentials_are_returned() throws Exception {
         kernel.launch();
         Thread.sleep(5000);
-        assertNotNull(kernel.getConfig().find(SETENV_CONFIG_NAMESPACE, "AWS_CONTAINER_CREDENTIALS_FULL_URI").getOnce());
-        assertEquals("Success: TES was success", new BufferedReader(new FileReader("./tesIntegTest.txt")).readLine());
-    }
-
-    @Test
-    void GIVEN_iot_role_alias_and_network_disabled_WHEN_tes_is_queried_THEN_correct_credentials_are_returned() throws Exception {
-        NetworkUtils networkUtils = NetworkUtils.getByPlatform();
-        networkUtils.disconnectMqtt();
-        kernel.launch();
-        Thread.sleep(5000);
-//        assertEquals("Success: TES was success", TESIntegrationTest.class.getResource("tesIntegTest.txt").toString());
-        assertNotNull(kernel.getConfig().find(SETENV_CONFIG_NAMESPACE, "AWS_CONTAINER_CREDENTIALS_FULL_URI").getOnce());
+        String url = kernel.getConfig().find(SETENV_CONFIG_NAMESPACE, "AWS_CONTAINER_CREDENTIALS_FULL_URI").getOnce().toString();
+        assertNotNull(url);
+        String credentials = Exec.sh(String.format("curl -s -o /dev/stdout %s", url));
+        assertThat(credentials, matchesPattern(
+                "\\{\"AccessKeyId\":\".+\",\"SecretAccessKey\":\".+\",\"Expiration\":\".+\",\"Token\":\".+\"\\}"));
     }
 
     void provision(Kernel kernel) throws IOException {
