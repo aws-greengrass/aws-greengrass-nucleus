d0ee8ac4fc29386c13d1eeae2fcc60a1326bd2cf
prateeky <prateeky@amazon.com>
2020-07-23 18:01:24 -0700

    Revert "rename principal to principle"
    This reverts commit ecaf782ff24c4e775a8653c1c483cdabf591b55e.

--- src/main/java/com/aws/iot/evergreen/auth/Permission.java
+++ src/main/java/com/aws/iot/evergreen/auth/Permission.java
@@ -10,7 +10,7 @@ import lombok.Value;
 @Builder
 @Value
 public class Permission {
-    @NonNull String principle;
+    @NonNull String principal;
     @NonNull String operation;
     String resource;
 }
