2a8e5b32131f3cd5eabe08371108b2ba823a029e
Hui Yang <hui-yang@users.noreply.github.com>
2020-08-10 18:49:01 -0700

    Set up Kernel alts and deployments directories (#339)
    * Set up Kernel alts and deployments directories
    
    * Add unit tests and address comments
    
    * Address comments and rebase
    
    * Rebase and address comments
    
    * Address comments and refactor serdes

--- src/main/java/com/aws/iot/evergreen/util/SerializerFactory.java
+++ src/main/java/com/aws/iot/evergreen/util/SerializerFactory.java
@@ -1,6 +1,7 @@
 package com.aws.iot.evergreen.util;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
@@ -9,9 +10,16 @@ public final class SerializerFactory {
     private static final ObjectMapper RECIPE_SERIALIZER =
             new ObjectMapper(new YAMLFactory()).configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES, true)
                     .setSerializationInclusion(JsonInclude.Include.NON_NULL);
+    private static final ObjectMapper JSON_OBJECT_MAPPER =
+            new ObjectMapper().configure(DeserializationFeature.FAIL_ON_INVALID_SUBTYPE, false)
+                    .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 
     public static ObjectMapper getRecipeSerializer() {
         return RECIPE_SERIALIZER;
+    }
+
+    public static ObjectMapper getJsonObjectMapper() {
+        return JSON_OBJECT_MAPPER;
     }
 
     private SerializerFactory() {
