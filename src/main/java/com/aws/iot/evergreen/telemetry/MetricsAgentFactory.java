fd1e7ff14d913726778524300382d9419da84485
saranya <nukai@amazon.com>
2020-08-24 14:48:37 -0700

    Use context for Executor, Move runnable code to another method, update emit metrics to match with sdk changes

--- src/main/java/com/aws/iot/evergreen/telemetry/MetricsAgentFactory.java
+++ src/main/java/com/aws/iot/evergreen/telemetry/MetricsAgentFactory.java
@@ -1,66 +1,73 @@
+/*
+ * Copyright Amazon.com Inc. or its affiliates.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 package com.aws.iot.evergreen.telemetry;
 
+import com.aws.iot.evergreen.dependency.Context;
 import com.aws.iot.evergreen.kernel.EvergreenService;
 import com.aws.iot.evergreen.kernel.Kernel;
 import com.aws.iot.evergreen.telemetry.api.MetricDataBuilder;
-import com.aws.iot.evergreen.telemetry.constants.DefaultMetricPeriod;
 import com.aws.iot.evergreen.telemetry.impl.Metric;
 import com.aws.iot.evergreen.telemetry.impl.MetricFactory;
+import com.aws.iot.evergreen.telemetry.models.TelemetryAggregation;
 import com.aws.iot.evergreen.telemetry.models.TelemetryMetricName;
 import com.aws.iot.evergreen.telemetry.models.TelemetryNamespace;
-import com.aws.iot.evergreen.telemetry.models.TelemetryType;
 import com.aws.iot.evergreen.telemetry.models.TelemetryUnit;
 
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
-public class MetricsAgentFactory {
+public  class MetricsAgentFactory {
+    private static final int DEFAULT_KERNEL_COMPONENTS_STATE_PERIOD = 300;
 
-    public void collectTimeBasedMetrics(Kernel kernel) {
-        this.collectKernelComponentState(kernel, DefaultMetricPeriod.DEFAULT_NUM_COMPONENT_STATE_PERIOD);
+    public void collectTimeBasedMetrics(Kernel kernel, Context context) {
+        this.collectKernelComponentState(kernel,context);
     }
 
-    private void collectKernelComponentState(Kernel kernel, int period) {
+    private void collectKernelComponentState(Kernel kernel, Context context) {
         Map<TelemetryMetricName, MetricDataBuilder> metricsMap = new HashMap<>();
         for (TelemetryMetricName telemetryMetricName : TelemetryMetricName.KernelComponents.values()) {
             Metric metric = Metric.builder()
-                    .metricNamespace(TelemetryNamespace.KERNEL)
+                    .metricNamespace(TelemetryNamespace.Kernel)
                     .metricName(telemetryMetricName)
-                    .metricUnit(TelemetryUnit.COUNT)
-                    .metricType(TelemetryType.TIME_BASED)
+                    .metricUnit(TelemetryUnit.Count)
+                    .metricAggregation(TelemetryAggregation.Sum)
                     .build();
             MetricDataBuilder metricDataBuilder = new MetricFactory().addMetric(metric);
             metricsMap.put(telemetryMetricName, metricDataBuilder);
         }
-
         Map<TelemetryMetricName, Integer> numComponentState = new HashMap<>();
         for (TelemetryMetricName telemetryMetricName : TelemetryMetricName.KernelComponents.values()) {
-                numComponentState.put(telemetryMetricName,0);
+            numComponentState.put(telemetryMetricName, 0);
         }
 
-        Runnable emitMetrics = new Runnable() {
-            @Override
-            public void run() {
-                Collection<EvergreenService> evergreenServices = kernel.orderedDependencies();
-                for (EvergreenService evergreenService : evergreenServices) {
-                    TelemetryMetricName telemetryMetricName = TelemetryMetricName.KernelComponents
-                            .valueOf("NUM_COMPONENTS_" + evergreenService.getState().toString());
-                    numComponentState.put(telemetryMetricName, numComponentState.get(telemetryMetricName) + 1);
-                }
+        ScheduledExecutorService executor = context.get(ScheduledExecutorService.class);
+        executor.scheduleAtFixedRate(emitMetrics(metricsMap, numComponentState, kernel), 0,
+                DEFAULT_KERNEL_COMPONENTS_STATE_PERIOD, TimeUnit.SECONDS);
 
-                for (HashMap.Entry<TelemetryMetricName, MetricDataBuilder> metricMap:metricsMap.entrySet()) {
-                    MetricDataBuilder metricDataBuilder = metricMap.getValue();
-                    metricDataBuilder.putMetricData(numComponentState.get(metricMap.getKey())).emit();
-                    numComponentState.put(metricMap.getKey(),0);
-                }
+    }
+
+    private Runnable emitMetrics(Map<TelemetryMetricName, MetricDataBuilder> metricsMap,
+                                 Map<TelemetryMetricName, Integer> numComponentState, Kernel kernel) {
+        return () -> {
+            Collection<EvergreenService> evergreenServices = kernel.orderedDependencies();
+            for (EvergreenService evergreenService : evergreenServices) {
+                String serviceState = evergreenService.getState().toString();
+                serviceState = serviceState.charAt(0) + serviceState.substring(1).toLowerCase();
+                TelemetryMetricName telemetryMetricName = TelemetryMetricName.KernelComponents
+                        .valueOf("NumberOfComponents" + serviceState);
+                numComponentState.put(telemetryMetricName, numComponentState.get(telemetryMetricName) + 1);
+            }
+            for (HashMap.Entry<TelemetryMetricName, MetricDataBuilder> metricMap : metricsMap.entrySet()) {
+                MetricDataBuilder metricDataBuilder = metricMap.getValue();
+                metricDataBuilder.putMetricData(numComponentState.get(metricMap.getKey())).emit();
+                numComponentState.put(metricMap.getKey(),0);
             }
         };
-
-        ScheduledExecutorService executor = Executors.newScheduledThreadPool(metricsMap.size());
-        executor.scheduleAtFixedRate(emitMetrics, 0, period, TimeUnit.SECONDS);
     }
 }
--- src/main/java/com/aws/iot/evergreen/telemetry/constants/DefaultMetricPeriod.java
+++
@@ -1,5 +0,0 @@
-package com.aws.iot.evergreen.telemetry.constants;
-
-public class DefaultMetricPeriod {
-    public static final int DEFAULT_NUM_COMPONENT_STATE_PERIOD = 300;
-}
