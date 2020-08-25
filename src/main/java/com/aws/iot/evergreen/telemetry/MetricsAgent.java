a3070f8e0be114a435e7a8da76eef7efe44a755f
saranya <nukai@amazon.com>
2020-08-25 14:17:33 -0700

    Add system metrics

--- src/main/java/com/aws/iot/evergreen/telemetry/MetricsAgent.java
+++ src/main/java/com/aws/iot/evergreen/telemetry/MetricsAgent.java
@@ -9,17 +9,14 @@ import com.aws.iot.evergreen.dependency.Context;
 import com.aws.iot.evergreen.dependency.ImplementsService;
 import com.aws.iot.evergreen.dependency.State;
 import com.aws.iot.evergreen.kernel.EvergreenService;
-import com.aws.iot.evergreen.kernel.Kernel;
 
 import javax.inject.Inject;
-
 
 @ImplementsService(name = MetricsAgent.METRICS_AGENT_SERVICE_TOPICS, version = "1.0.0", autostart = true)
 public class MetricsAgent extends EvergreenService {
     public static final String METRICS_AGENT_SERVICE_TOPICS = "MetricsAgent";
     private final MetricsAgentFactory metricsAgentFactory = new MetricsAgentFactory();
-    @Inject
-    private Kernel kernel;
+
     @Inject
     private Context context;
 
@@ -30,7 +27,7 @@ public class MetricsAgent extends EvergreenService {
     @Override
     protected void startup() {
         reportState(State.RUNNING);
-        this.metricsAgentFactory.collectTimeBasedMetrics(this.kernel,this.context);
+        this.metricsAgentFactory.collectSystemMetrics(this.context);
     }
 
     @Override
--- src/main/java/com/aws/iot/evergreen/telemetry/MetricsAgentFactory.java
+++ src/main/java/com/aws/iot/evergreen/telemetry/MetricsAgentFactory.java
@@ -6,8 +6,6 @@
 package com.aws.iot.evergreen.telemetry;
 
 import com.aws.iot.evergreen.dependency.Context;
-import com.aws.iot.evergreen.kernel.EvergreenService;
-import com.aws.iot.evergreen.kernel.Kernel;
 import com.aws.iot.evergreen.telemetry.api.MetricDataBuilder;
 import com.aws.iot.evergreen.telemetry.impl.Metric;
 import com.aws.iot.evergreen.telemetry.impl.MetricFactory;
@@ -15,58 +13,80 @@ import com.aws.iot.evergreen.telemetry.models.TelemetryAggregation;
 import com.aws.iot.evergreen.telemetry.models.TelemetryMetricName;
 import com.aws.iot.evergreen.telemetry.models.TelemetryNamespace;
 import com.aws.iot.evergreen.telemetry.models.TelemetryUnit;
+import oshi.SystemInfo;
+import oshi.hardware.CentralProcessor;
 
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 public  class MetricsAgentFactory {
-    private static final int DEFAULT_KERNEL_COMPONENTS_STATE_PERIOD = 300;
+    private static final int DEFAULT_SYSTEM_METRICS_PERIOD = 300;
+    private static int MB_CONVERTER = 1024 * 1024;
+    private static int PERCENTAGE_CONVERTER = 100;
+    private static CentralProcessor cpu = new SystemInfo().getHardware().getProcessor();
+    private static SystemInfo systemInfo = new SystemInfo();
+    private static long[] previousTicks = new long[CentralProcessor.TickType.values().length];
 
-    public void collectTimeBasedMetrics(Kernel kernel, Context context) {
-        this.collectKernelComponentState(kernel,context);
-    }
 
-    private void collectKernelComponentState(Kernel kernel, Context context) {
+    /**
+     * Kuch bhi.
+     * @param context used in executor
+     */
+    public void collectSystemMetrics(Context context) {
         Map<TelemetryMetricName, MetricDataBuilder> metricsMap = new HashMap<>();
+        Metric systemMetric = Metric.builder()
+                .metricNamespace(TelemetryNamespace.SystemMetrics)
+                .metricName(TelemetryMetricName.SystemMetrics.CpuUsage)
+                .metricUnit(TelemetryUnit.Percent)
+                .metricAggregation(TelemetryAggregation.Average)
+                .build();
+        MetricDataBuilder mdb = new MetricFactory().addMetric(systemMetric);
+        metricsMap.put(TelemetryMetricName.SystemMetrics.CpuUsage, mdb);
+
+        systemMetric = Metric.builder()
+                .metricNamespace(TelemetryNamespace.SystemMetrics)
+                .metricName(TelemetryMetricName.SystemMetrics.TotalNumberOfFDs)
+                .metricUnit(TelemetryUnit.Count)
+                .metricAggregation(TelemetryAggregation.Average)
+                .build();
+        mdb = new MetricFactory().addMetric(systemMetric);
+        metricsMap.put(TelemetryMetricName.SystemMetrics.TotalNumberOfFDs, mdb);
+
+        systemMetric = Metric.builder()
+                .metricNamespace(TelemetryNamespace.SystemMetrics)
+                .metricName(TelemetryMetricName.SystemMetrics.SystemMemUsage)
+                .metricUnit(TelemetryUnit.Megabytes)
+                .metricAggregation(TelemetryAggregation.Average)
+                .build();
+        mdb = new MetricFactory().addMetric(systemMetric);
+        metricsMap.put(TelemetryMetricName.SystemMetrics.SystemMemUsage, mdb);
+
+        Map<TelemetryMetricName, Object> systemMetrics = new HashMap<>();
         for (TelemetryMetricName telemetryMetricName : TelemetryMetricName.KernelComponents.values()) {
-            Metric metric = Metric.builder()
-                    .metricNamespace(TelemetryNamespace.Kernel)
-                    .metricName(telemetryMetricName)
-                    .metricUnit(TelemetryUnit.Count)
-                    .metricAggregation(TelemetryAggregation.Sum)
-                    .build();
-            MetricDataBuilder metricDataBuilder = new MetricFactory().addMetric(metric);
-            metricsMap.put(telemetryMetricName, metricDataBuilder);
-        }
-        Map<TelemetryMetricName, Integer> numComponentState = new HashMap<>();
-        for (TelemetryMetricName telemetryMetricName : TelemetryMetricName.KernelComponents.values()) {
-            numComponentState.put(telemetryMetricName, 0);
+            systemMetrics.put(telemetryMetricName, 0);
         }
 
         ScheduledExecutorService executor = context.get(ScheduledExecutorService.class);
-        executor.scheduleAtFixedRate(emitMetrics(metricsMap, numComponentState, kernel), 0,
-                DEFAULT_KERNEL_COMPONENTS_STATE_PERIOD, TimeUnit.SECONDS);
-
+        executor.scheduleAtFixedRate(emitMetrics(metricsMap, systemMetrics), 0,
+                DEFAULT_SYSTEM_METRICS_PERIOD, TimeUnit.SECONDS);
     }
 
     private Runnable emitMetrics(Map<TelemetryMetricName, MetricDataBuilder> metricsMap,
-                                 Map<TelemetryMetricName, Integer> numComponentState, Kernel kernel) {
+                                 Map<TelemetryMetricName, Object> systemMetrics) {
         return () -> {
-            Collection<EvergreenService> evergreenServices = kernel.orderedDependencies();
-            for (EvergreenService evergreenService : evergreenServices) {
-                String serviceState = evergreenService.getState().toString();
-                serviceState = serviceState.charAt(0) + serviceState.substring(1).toLowerCase();
-                TelemetryMetricName telemetryMetricName = TelemetryMetricName.KernelComponents
-                        .valueOf("NumberOfComponents" + serviceState);
-                numComponentState.put(telemetryMetricName, numComponentState.get(telemetryMetricName) + 1);
-            }
+            systemMetrics.put(TelemetryMetricName.SystemMetrics.CpuUsage,
+                    cpu.getSystemCpuLoadBetweenTicks(previousTicks) * PERCENTAGE_CONVERTER);
+            previousTicks = cpu.getSystemCpuLoadTicks();
+            systemMetrics.put(TelemetryMetricName.SystemMetrics.TotalNumberOfFDs,
+                    systemInfo.getOperatingSystem().getFileSystem().getOpenFileDescriptors());
+            systemMetrics.put(TelemetryMetricName.SystemMetrics.SystemMemUsage,
+                    systemInfo.getHardware().getMemory().getVirtualMemory().getVirtualInUse() / MB_CONVERTER);
             for (HashMap.Entry<TelemetryMetricName, MetricDataBuilder> metricMap : metricsMap.entrySet()) {
                 MetricDataBuilder metricDataBuilder = metricMap.getValue();
-                metricDataBuilder.putMetricData(numComponentState.get(metricMap.getKey())).emit();
-                numComponentState.put(metricMap.getKey(),0);
+                metricDataBuilder.putMetricData(systemMetrics.get(metricMap.getKey())).emit();
+                systemMetrics.put(metricMap.getKey(),0);
             }
         };
     }
