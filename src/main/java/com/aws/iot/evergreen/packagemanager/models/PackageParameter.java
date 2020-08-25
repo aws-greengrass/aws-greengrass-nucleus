9bedadefffc2234d434d82df8fa8d104c0ee7e96
Michael Dombrowski <mdombro@amazon.com>
2020-08-24 17:44:43 -0700

    NPE protection for merging config

--- src/main/java/com/aws/iot/evergreen/packagemanager/models/PackageParameter.java
+++ src/main/java/com/aws/iot/evergreen/packagemanager/models/PackageParameter.java
@@ -3,8 +3,10 @@ package com.aws.iot.evergreen.packagemanager.models;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NonNull;
 
 import java.util.Collections;
 import java.util.HashSet;
@@ -14,8 +16,10 @@ import java.util.Set;
 @Getter
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @JsonSerialize
+@AllArgsConstructor
 public class PackageParameter {
 
+    @NonNull
     @EqualsAndHashCode.Include
     private final String name;
 
@@ -36,20 +40,6 @@ public class PackageParameter {
     public PackageParameter(@JsonProperty("name") String name, @JsonProperty("value") String value,
                             @JsonProperty("type") String type) {
         this(name, value, ParameterType.valueOf(type.toUpperCase()));
-    }
-
-    /**
-     * Create a Package Param object.
-     *
-     * @param name  Name of the parameter
-     * @param value Default value for the parameter
-     * @param type  Parameter Type enum value
-     */
-    public PackageParameter(String name, String value, ParameterType type) {
-        this.name = name;
-        this.type = type;
-        // TODO: Validate type and initialize corresponding type here?
-        this.value = value;
     }
 
     /**
