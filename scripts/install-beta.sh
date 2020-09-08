22be5a3e313501dc44848dec5eb084e37cb8507a
Tilo Chen <tiangac@amazon.com>
2020-09-08 12:21:37 -0400

    add beta install script for linux

---
+++ scripts/install-beta.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+
+# defaults
+GG_ROOT_DIR="/greengrass/v2"
+HTTP_INSTALL_DIR="$GG_ROOT_DIR/plugins/trusted"
+FRESH_INSTALL=false
+
+# expected files
+NUCLEUS_ZIP="aws.greengrass.nucleus.zip"
+CLI_ZIP="aws-greengrass-cli.zip"
+CLI_DIR="evergreen-cli-1.0-SNAPSHOT"
+HTTP_JAR="aws-greengrass-http.jar"
+
+# make sure required file/directory exist
+for i in $GG_ROOT_DIR $NUCLEUS_ZIP $CLI_ZIP $HTTP_JAR ; do
+  if [ ! -e $i ] ; then
+    echo "Install failed. $i not found"
+    exit 1
+  fi
+done
+
+# get arguments
+for arg in "$@" ; do
+  case $arg in
+    -r|--root)
+    GG_ROOT_DIR=$2
+    HTTP_INSTALL_DIR="$GG_ROOT_DIR/plugins/trusted"
+    shift
+    ;;
+    -f|--fresh-install)
+    FRESH_INSTALL=true
+    shift
+    ;;
+  esac
+done
+
+# unzip
+(unzip $CLI_ZIP && unzip $NUCLEUS_ZIP) || { echo "Install failed. Cannot unzip"; exit 1; }
+
+# remove existing installation if requested
+if "$FRESH_INSTALL" && [ -d "$GG_ROOT_DIR" ]; then
+  echo 'Removing old installation'
+  rm -rf "$GG_ROOT_DIR"
+fi
+
+# install CLI if not already
+if ! type 'greengrass-cli' &> /dev/null; then
+  echo "Installing Greengrass CLI"
+  sudo bash "$CLI_DIR/install.sh" || { echo 'Install failed. cannot install CLI'; exit 1; }
+else
+  echo "Greengrass CLI already installed"
+fi
+
+# copy over local dashboard jar
+mkdir -p "$HTTP_INSTALL_DIR" || { echo 'Install failed. cannot create local dashboard directory'; exit 1; }
+cp $HTTP_JAR "$HTTP_INSTALL_DIR/"
+echo "local dashboard installed"
+
+echo "Installation complete"
