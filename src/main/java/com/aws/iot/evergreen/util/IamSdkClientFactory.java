a2cd3693a0b51095b263bfa01b39a7337d165ff2
Michael Dombrowski <mdombro@amazon.com>
2020-09-16 14:42:13 -0700

    Use Jackson instead of custom serializer in tlog (#428)
    * Use Jackson instead of custom serializer
    
    * Move E2E tests to separate workflow, add IamException as retryable for e2e failures

--- src/main/java/com/aws/iot/evergreen/util/IamSdkClientFactory.java
+++ src/main/java/com/aws/iot/evergreen/util/IamSdkClientFactory.java
@@ -8,6 +8,7 @@ import software.amazon.awssdk.core.retry.conditions.RetryCondition;
 import software.amazon.awssdk.core.retry.conditions.RetryOnExceptionsCondition;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.iam.IamClient;
+import software.amazon.awssdk.services.iam.model.IamException;
 import software.amazon.awssdk.services.iam.model.LimitExceededException;
 import software.amazon.awssdk.services.iam.model.ServiceFailureException;
 
@@ -19,14 +20,14 @@ import java.util.Set;
  * Accessor for AWS IAM SDK.
  */
 public final class IamSdkClientFactory {
-    private static final Set<Class<? extends Exception>> retryableIamExceptions =
-            new HashSet<>(Arrays.asList(LimitExceededException.class, ServiceFailureException.class));
+    private static final Set<Class<? extends Exception>> retryableIamExceptions = new HashSet<>(
+            Arrays.asList(IamException.class, LimitExceededException.class, ServiceFailureException.class));
 
     private static final RetryCondition retryCondition = OrRetryCondition
             .create(RetryCondition.defaultRetryCondition(), RetryOnExceptionsCondition.create(retryableIamExceptions));
 
     private static final RetryPolicy retryPolicy =
-            RetryPolicy.builder().numRetries(5).backoffStrategy(BackoffStrategy.defaultStrategy())
+            RetryPolicy.builder().numRetries(5).backoffStrategy(BackoffStrategy.defaultThrottlingStrategy())
                     .retryCondition(retryCondition).build();
 
     private IamSdkClientFactory() {
