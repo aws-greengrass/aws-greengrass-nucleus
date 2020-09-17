a5016eb279c281de9ae9001238df4764dac9edf2
fahadmohammed01 <52720275+fahadmohammed01@users.noreply.github.com>
2020-09-17 16:43:32 -0700

    updated unit and integ tests (#446)
    * updated unit and integ tests
    
    * addressed comments

--- src/test/java/com/aws/iot/evergreen/kernel/EvergreenServiceTest.java
+++ src/test/java/com/aws/iot/evergreen/kernel/EvergreenServiceTest.java
@@ -3,31 +3,79 @@
 
 package com.aws.iot.evergreen.kernel;
 
-import com.aws.iot.evergreen.config.Validator;
+import com.amazon.aws.iot.greengrass.component.common.DependencyType;
+import com.aws.iot.evergreen.config.Configuration;
+import com.aws.iot.evergreen.config.Topic;
+import com.aws.iot.evergreen.config.Topics;
+
+import com.aws.iot.evergreen.dependency.Context;
 import com.aws.iot.evergreen.dependency.State;
+import com.aws.iot.evergreen.kernel.exceptions.ServiceLoadException;
 import com.aws.iot.evergreen.testcommons.testutilities.EGServiceTestUtil;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Captor;
-import org.mockito.Mockito;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+
+import static com.aws.iot.evergreen.kernel.EvergreenService.PRIVATE_STORE_NAMESPACE_TOPIC;
+import static com.aws.iot.evergreen.kernel.EvergreenService.SERVICES_NAMESPACE_TOPIC;
+import static com.aws.iot.evergreen.kernel.EvergreenService.SERVICE_DEPENDENCIES_NAMESPACE_TOPIC;
 import static com.aws.iot.evergreen.kernel.Lifecycle.STATE_TOPIC_NAME;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.Mockito.lenient;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class EvergreenServiceTest extends EGServiceTestUtil {
 
-    private EvergreenService evergreenService;
-
-    @Captor
-    private ArgumentCaptor<Validator> validatorArgumentCaptor;
+    @Mock
+    private Kernel kernel;
+    private Context context;
+    private Configuration configuration;
+    private EvergreenService aService;
+    private EvergreenService bService;
+    private EvergreenService cService;
+    private EvergreenService dService;
+    private EvergreenService eService;
 
     @BeforeEach
-    void beforeEach() {
-        evergreenService = new EvergreenService(initializeMockedConfig());
+    void beforeEach() throws IOException, URISyntaxException, ServiceLoadException {
+        Path configPath = Paths.get(this.getClass().getResource("services.yaml").toURI());
+        context = spy(new Context());
+        when(context.get(Kernel.class)).thenReturn(kernel);
+        configuration = new Configuration(context);
+        configuration.read(configPath);
+        Topics root = configuration.getRoot();
+        bService = new EvergreenService(root.findTopics(SERVICES_NAMESPACE_TOPIC, "B"));
+        cService = new EvergreenService(root.findTopics(SERVICES_NAMESPACE_TOPIC, "C"));
+        dService = new EvergreenService(root.findTopics(SERVICES_NAMESPACE_TOPIC, "D"));
+        eService = new EvergreenService(root.findTopics(SERVICES_NAMESPACE_TOPIC, "E"));
+        when(kernel.locate("B")).thenReturn(bService);
+        when(kernel.locate("C")).thenReturn(cService);
+        when(kernel.locate("D")).thenReturn(dService);
+        lenient().when(kernel.locate("E")).thenReturn(eService);
+        aService = spy(new EvergreenService(root.findTopics(SERVICES_NAMESPACE_TOPIC, "A")));
+
+    }
+
+    @AfterEach
+    void afterEach() throws IOException {
+        context.close();
     }
 
     @Test
@@ -40,27 +88,41 @@ public class EvergreenServiceTest extends EGServiceTestUtil {
 
         // THEN
         // verify config
-        Assertions.assertSame(config, evergreenService.config);
-        Mockito.verify(privateStoreTopic).createLeafChild(STATE_TOPIC_NAME);
+        Assertions.assertSame(configuration.getRoot().findTopics(SERVICES_NAMESPACE_TOPIC, "A"), aService.config);
 
-        // verify stateTopic
-        Mockito.verify(stateTopic).withParentNeedsToKnow(false);
-        Mockito.verify(stateTopic).withValue(State.NEW);
-        Mockito.verify(stateTopic).addValidator(validatorArgumentCaptor.capture());
-        Mockito.verifyNoMoreInteractions(stateTopic);
+        //verify dependencies are set up right
+        assertEquals(aService.dependencies.size(), 3);
+        assertEquals(aService.dependencies.get(bService).dependencyType, DependencyType.HARD);
+        assertEquals(aService.dependencies.get(cService).dependencyType, DependencyType.SOFT);
+        assertEquals(aService.dependencies.get(dService).dependencyType, DependencyType.HARD);
 
-        // verify validator
-        Validator validator = validatorArgumentCaptor.getValue();
-        State returnedState = (State) validator.validate(State.NEW, null);
-        Assertions.assertSame(State.NEW, returnedState);
+        //verify state is NEW
+        Topic stateTopic = aService.getConfig().find(PRIVATE_STORE_NAMESPACE_TOPIC, STATE_TOPIC_NAME);
+        assertEquals(stateTopic.getOnce(), State.NEW);
+        assertFalse(stateTopic.parentNeedsToKnow());
+
     }
 
     @Test
-    void GIVEN_a_new_state_WHEN_getState_THEN_return_the_new_state() {
-        Mockito.when(stateTopic.getOnce()).thenReturn(State.NEW);
+    void GIVEN_service_WHEN_dependencies_change_THEN_service_restarts() {
+        //GIVEN service A with dependencies B,C,D
+        // provided in the beforeEach
 
-        Assertions.assertSame(State.NEW, evergreenService.getState());
+        //WHEN D is removed and E is added
+        Topic topic = aService.getConfig().find(SERVICE_DEPENDENCIES_NAMESPACE_TOPIC);
+        topic.withNewerValue(System.currentTimeMillis(), Arrays.asList("B", "C", "E"));
+        context.runOnPublishQueueAndWait(() -> {
+        });
+        // THEN
+        // verify D is removed and E is added
+        assertEquals(aService.dependencies.size(), 3);
+        assertNull(aService.dependencies.get(dService));
+        assertNotNull(aService.dependencies.get(eService));
 
-        Mockito.verify(stateTopic).getOnce();
+        aService.getState();
+        //verify service is restarted
+        // clearODcache is called 3 times when B,C,D is added, once when D is removed and 2 times when B,C was retained
+        //and once when E is added.
+        verify(kernel, times(7)).clearODcache();
     }
 }
