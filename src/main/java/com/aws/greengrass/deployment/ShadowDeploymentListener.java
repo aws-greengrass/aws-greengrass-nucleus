963130c63aff7f3dcd83517673e37682388d2140
Mohammed <faham@amazon.com>
2020-10-01 21:24:07 -0700

    minor bug fix

--- src/main/java/com/aws/greengrass/deployment/ShadowDeploymentListener.java
+++ src/main/java/com/aws/greengrass/deployment/ShadowDeploymentListener.java
@@ -11,6 +11,7 @@ import com.aws.greengrass.util.Coerce;
 import com.aws.greengrass.util.Pair;
 import com.aws.greengrass.util.SerializerFactory;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import lombok.Getter;
 import lombok.Setter;
 import software.amazon.awssdk.crt.mqtt.MqttClientConnection;
@@ -46,6 +47,7 @@ public class ShadowDeploymentListener implements InjectionActions {
     private static final long TIMEOUT_FOR_PUBLISHING_TO_TOPICS_SECONDS = Duration.ofMinutes(1).getSeconds();
     private static final long WAIT_TIME_TO_SUBSCRIBE_AGAIN_IN_MS = Duration.ofMinutes(2).toMillis();
     private static final Logger logger = LogManager.getLogger(ShadowDeploymentListener.class);
+    public static final String CONFIGURATION_ARN_LOG_KEY_NAME = "CONFIGURATION_ARN";
     private final Queue<Pair<String, Map<String, Object>>> desiredStateQueue = new ConcurrentLinkedQueue<>();
     @Inject
     private DeploymentQueue deploymentQueue;
@@ -81,7 +83,7 @@ public class ShadowDeploymentListener implements InjectionActions {
     @Override
     public void postInject() {
 
-        if (deviceConfiguration.isDeviceConfiguredToTalkToCloud()) {
+        if (!deviceConfiguration.isDeviceConfiguredToTalkToCloud()) {
             logger.atWarn().log("Device not configured to talk to AWS Iot cloud. Device will run in offline mode");
             return;
         }
@@ -162,6 +164,7 @@ public class ShadowDeploymentListener implements InjectionActions {
         iotShadowClient.PublishGetShadow(getShadowRequest, QualityOfService.AT_LEAST_ONCE);
     }
 
+    @SuppressFBWarnings
     private Boolean deploymentStatusChanged(Map<String, Object> deploymentDetails) {
         DeploymentStatus status = DeploymentStatus.valueOf((String)
                 deploymentDetails.get(DEPLOYMENT_STATUS_KEY_NAME));
@@ -170,11 +173,18 @@ public class ShadowDeploymentListener implements InjectionActions {
         // only update reported state when the deployment succeeds.
         if (DeploymentStatus.SUCCEEDED.equals(status)) {
 
-            Pair<String, Map<String, Object>> desired = desiredStateQueue.remove();
+            Pair<String, Map<String, Object>> desired = desiredStateQueue.peek();
             // discard configurations that might have got added to the queue but the deployment
             // got discarded before being processed due to a new shadow deployment
-            while (!desired.getLeft().equals(configurationArn)) {
-                desired = desiredStateQueue.remove();
+            while (desired != null && !desired.getLeft().equals(configurationArn)) {
+                desiredStateQueue.poll();
+                desired = desiredStateQueue.peek();
+            }
+
+            if (desired == null) {
+                logger.atError().kv(CONFIGURATION_ARN_LOG_KEY_NAME, configurationArn)
+                        .log("Unable to update shadow for deployment");
+                return true;
             }
 
             try {
@@ -185,6 +195,9 @@ public class ShadowDeploymentListener implements InjectionActions {
                 updateShadowRequest.state = shadowState;
                 iotShadowClient.PublishUpdateShadow(updateShadowRequest, QualityOfService.AT_LEAST_ONCE)
                         .get(TIMEOUT_FOR_PUBLISHING_TO_TOPICS_SECONDS, TimeUnit.SECONDS);
+                desiredStateQueue.remove();
+                logger.atInfo().kv(CONFIGURATION_ARN_LOG_KEY_NAME, configurationArn)
+                        .log("Updated reported state for deployment");
                 return true;
             } catch (InterruptedException e) {
                 //Since this method can run as runnable cannot throw exception so handling exceptions here
@@ -207,13 +220,13 @@ public class ShadowDeploymentListener implements InjectionActions {
         String configurationArn = (String) configuration.get("configurationArn");
         synchronized (ShadowDeploymentListener.class) {
             if (lastVersion != null && lastVersion > version) {
-                logger.atInfo().kv("CONFIGURATION_ARN", configurationArn)
+                logger.atInfo().kv(CONFIGURATION_ARN_LOG_KEY_NAME, configurationArn)
                         .kv("SHADOW_VERSION", version)
                         .log("Old deployment notification, Ignoring...");
                 return;
             }
             if (lastConfigurationArn != null && lastConfigurationArn.equals(configurationArn)) {
-                logger.atInfo().kv("CONFIGURATION_ARN", configurationArn)
+                logger.atInfo().kv(CONFIGURATION_ARN_LOG_KEY_NAME, configurationArn)
                         .log("Duplicate deployment notification, Ignoring...");
                 return;
             }
