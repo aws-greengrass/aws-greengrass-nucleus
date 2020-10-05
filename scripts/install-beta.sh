74d4350675898ecf91c61b97b7b9e38ef81aafdc
Tilo Chen <tiangac@amazon.com>
2020-10-05 16:43:19 -0400

    address comments

--- scripts/install-beta.sh
+++ scripts/install-beta.sh
@@ -1,9 +1,17 @@
-#!/usr/bin/env bash
+#!/bin/sh
+# If no options provided, will unzip and install the CLI and local dashboard
+#
+# OPTIONS
+# [-r|--root] <root dir>: specify Greengrass root directory for installation
+# [-f|--fresh-install]: delete previous Greengrass installation if exists
+# [-ld|--launch-default]: after install, launch Greengrass with default params for provision and running
 
 # defaults
 GG_ROOT_DIR="/greengrass/v2"
 HTTP_INSTALL_DIR="$GG_ROOT_DIR/plugins/trusted"
 FRESH_INSTALL=false
+LAUNCH_WITH_DEFAULT=false
+AWS_REGION="us-east-1"
 
 # expected files
 NUCLEUS_ZIP="aws.greengrass.nucleus.zip"
@@ -31,6 +39,10 @@ for arg in "$@" ; do
     FRESH_INSTALL=true
     shift
     ;;
+    -ld|--launch-default)
+    LAUNCH_WITH_DEFAULT=true
+    shift
+    ;;
   esac
 done
 
@@ -39,20 +51,21 @@ done
 
 # remove existing installation if requested
 if "$FRESH_INSTALL" && [ -d "$GG_ROOT_DIR" ]; then
-  echo "Attempting fresh install. This will delete existing directory: $GG_ROOT_DIR"
-  read -r -p "Are you sure (y/n)?" choice
+  echo "Attempting fresh install. This will delete the existing directory: $GG_ROOT_DIR"
+  echo "Are you sure (y/n)?"
+  read -r choice
   case "$choice" in
-    y|Y ) rm -rf "$GG_ROOT_DIR";;
-    n|N ) echo "Aborted.";;
+    y|Y) rm -rf "$GG_ROOT_DIR";;
+    *) echo "Aborted.";;
   esac
 fi
 
 # install CLI if not already
-if ! type 'greengrass-cli' &> /dev/null; then
-  echo "Installing Greengrass CLI"
-  sudo bash "$CLI_DIR/install.sh" || { echo 'Install failed. cannot install CLI'; exit 1; }
-else
+if type 'greengrass-cli' > /dev/null 2>&1; then
   echo "Greengrass CLI already installed"
+else
+  echo "Installing Greengrass CLI"
+  sudo ./"$CLI_DIR/install.sh" || { echo 'Install failed. cannot install CLI'; exit 1; }
 fi
 
 # copy over local dashboard jar
@@ -60,4 +73,12 @@ mkdir -p "$HTTP_INSTALL_DIR" || { echo 'Install failed. cannot create local dash
 cp $HTTP_JAR "$HTTP_INSTALL_DIR/"
 echo "local dashboard installed"
 
-echo "Installation complete"
+echo "Installation complete. Installed at: $GG_ROOT_DIR"
+
+if "$LAUNCH_WITH_DEFAULT"; then
+  echo "Starting Greengrass"
+  java -Droot="$GG_ROOT_DIR" -Dlog.store=FILE -jar lib/Greengrass.jar \
+    --aws-region "$AWS_REGION" --provision true --setup-tes true
+else
+  echo "You can now start Greengrass"
+fi
