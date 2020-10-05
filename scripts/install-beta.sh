b5619bd18eb19f441c5a4dceda974d6dd26c5231
Tilo Chen <tiangac@amazon.com>
2020-10-05 13:26:48 -0400

    minor change

--- scripts/install-beta.sh
+++ scripts/install-beta.sh
@@ -12,7 +12,7 @@ CLI_DIR="evergreen-cli-1.0-SNAPSHOT"
 HTTP_JAR="aws-greengrass-http.jar"
 
 # make sure required file/directory exist
-for i in $GG_ROOT_DIR $NUCLEUS_ZIP $CLI_ZIP $HTTP_JAR ; do
+for i in $NUCLEUS_ZIP $CLI_ZIP $HTTP_JAR ; do
   if [ ! -e $i ] ; then
     echo "Install failed. $i not found"
     exit 1
@@ -39,8 +39,12 @@ done
 
 # remove existing installation if requested
 if "$FRESH_INSTALL" && [ -d "$GG_ROOT_DIR" ]; then
-  echo 'Removing old installation'
-  rm -rf "$GG_ROOT_DIR"
+  echo "Attempting fresh install. This will delete existing directory: $GG_ROOT_DIR"
+  read -r -p "Are you sure (y/n)?" choice
+  case "$choice" in
+    y|Y ) rm -rf "$GG_ROOT_DIR";;
+    n|N ) echo "Aborted.";;
+  esac
 fi
 
 # install CLI if not already
