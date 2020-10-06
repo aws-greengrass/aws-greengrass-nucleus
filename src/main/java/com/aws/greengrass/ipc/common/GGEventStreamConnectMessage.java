14b9159e5fe701a46416a25df0889bd9b41ea0ad
Banthiya <abanthiy@amazon.com>
2020-10-05 23:50:14 -0700

    Making lifecycle APIs work

--- src/main/java/com/aws/greengrass/ipc/common/GGEventStreamConnectMessage.java
+++ src/main/java/com/aws/greengrass/ipc/common/GGEventStreamConnectMessage.java
@@ -1,10 +1,16 @@
 package com.aws.greengrass.ipc.common;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Builder
 @Data
+@JsonSerialize
+@NoArgsConstructor
+@AllArgsConstructor
 public class GGEventStreamConnectMessage {
     String authToken;
 }
