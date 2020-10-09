6ee8b1845c88ae3a9f161485486b8f6d37ce73d1
Xueli Zheng <52835750+ShirleyZheng92@users.noreply.github.com>
2020-10-08 23:41:31 -0700

    Add configurationUpdate in LocalDeploymentRequest (#500)
    * Add configurationUpdate in LocalDeploymentRequest
    
    * Add more tests
    
    Co-authored-by: Xueli Zheng <shirlez@amazon.com>
    Co-authored-by: Ethan Huang <shuyeh@amazon.com>

---
+++ src/main/java/com/aws/greengrass/deployment/model/ConfigurationUpdateOperation.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.aws.greengrass.deployment.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+import java.util.List;
+import java.util.Map;
+
+@Data
+public class ConfigurationUpdateOperation {
+
+    @JsonProperty("MERGE")
+    Map<String, Object> valueToMerge;
+
+    @JsonProperty("RESET")
+    List<String> pathsToReset;
+}
