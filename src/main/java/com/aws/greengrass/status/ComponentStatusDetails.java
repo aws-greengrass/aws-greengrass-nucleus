76b1d10852d215eba678b9a46066a813cc1b82c4
Nikkhil Muthye <nikkhim@amazon.com>
2020-10-25 21:38:32 -0700

    Add Serialization tag for boolean to set the field name correctly.

--- src/main/java/com/aws/greengrass/status/ComponentStatusDetails.java
+++ src/main/java/com/aws/greengrass/status/ComponentStatusDetails.java
@@ -27,6 +27,8 @@ public class ComponentStatusDetails {
 
     private String statusDetails;
 
+    // We need to add this since during serialization, the 'is' is removed.
+    @JsonProperty("isRoot")
     private boolean isRoot;
 
     @JsonProperty("status")
