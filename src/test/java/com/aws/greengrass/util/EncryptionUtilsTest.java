d94b634b6f5802dcea8c37dcb9af9c2c5eabc4b0
Jason <zhengang@amazon.com>
2020-10-26 19:21:41 -0700

    add copyright headers

--- src/test/java/com/aws/greengrass/util/EncryptionUtilsTest.java
+++ src/test/java/com/aws/greengrass/util/EncryptionUtilsTest.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 package com.aws.greengrass.util;
 
 import org.apache.commons.codec.binary.Base64;
