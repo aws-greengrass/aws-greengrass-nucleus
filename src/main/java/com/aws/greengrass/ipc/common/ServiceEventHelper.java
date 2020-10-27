955509fe6918914052e45e5b396e693572a6fc63
Joe Butler <joebutl@amazon.com>
2020-10-26 19:01:14 -0700

    Enforce copyright headers (#539)
    * Add license-maven-plugin
    
    * Run mvn license:format

--- src/main/java/com/aws/greengrass/ipc/common/ServiceEventHelper.java
+++ src/main/java/com/aws/greengrass/ipc/common/ServiceEventHelper.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 package com.aws.greengrass.ipc.common;
 
 import com.aws.greengrass.ipc.ConnectionContext;
--- src/test/java/com/aws/greengrass/dependency/ContextDependencyInjectionTest.java
+++ src/test/java/com/aws/greengrass/dependency/ContextDependencyInjectionTest.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 package com.aws.greengrass.dependency;
 
 import org.junit.jupiter.api.AfterEach;
