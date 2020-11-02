1e11d786b8cd12e4032f715ea823441e7ec92ffe
Nikkhil Muthye <nikkhim@amazon.com>
2020-11-02 09:35:01 -0800

    Subscribe to correct thing name topic in FSS and telemetry agent. (#603)

--- src/main/java/com/aws/greengrass/status/FleetStatusService.java
+++ src/main/java/com/aws/greengrass/status/FleetStatusService.java
@@ -124,7 +124,7 @@ public class FleetStatusService extends GreengrassService {
         this.platform = System.getProperty("os.name");
 
         updateThingNameAndPublishTopic(Coerce.toString(deviceConfiguration.getThingName()));
-        topics.lookup(DeviceConfiguration.DEVICE_PARAM_THING_NAME)
+        deviceConfiguration.getThingName()
                 .subscribe((why, node) -> updateThingNameAndPublishTopic(Coerce.toString(node)));
 
         topics.lookup(PARAMETERS_CONFIG_KEY, FLEET_STATUS_PERIODIC_UPDATE_INTERVAL_SEC)
