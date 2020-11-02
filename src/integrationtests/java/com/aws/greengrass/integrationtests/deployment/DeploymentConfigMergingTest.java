abe3a58e7deb755760ae7f93f9fe8a346eac9474
Michael Dombrowski <mdombro@amazon.com>
2020-11-02 12:03:24 -0800

    Log newly added Topics in the tlog, even if no leaf nodes (#602)
    * Log newly added Topics in the tlog, even if no leaf nodes
    
    * TLog save null values too

--- src/integrationtests/java/com/aws/greengrass/integrationtests/deployment/DeploymentConfigMergingTest.java
+++ src/integrationtests/java/com/aws/greengrass/integrationtests/deployment/DeploymentConfigMergingTest.java
@@ -476,7 +476,7 @@ class DeploymentConfigMergingTest extends BaseITCase {
         GreengrassService sleeperB = kernel.locate("sleeperB");
         assertEquals(State.RUNNING, sleeperB.getState());
         // ensure context finish all tasks
-        kernel.getContext().runOnPublishQueueAndWait(() -> {});
+        kernel.getContext().waitForPublishQueueToClear();
         // ensuring config value for sleeperA is removed
         assertFalse(kernel.getConfig().findTopics(SERVICES_NAMESPACE_TOPIC).children.containsKey("sleeperA"),
                 "sleeperA removed");
