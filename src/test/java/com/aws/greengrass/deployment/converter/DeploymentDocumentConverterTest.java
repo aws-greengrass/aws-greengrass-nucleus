ab11e122aa1954a911c4c287c1aee85c5c2e65ac
awszztt <66716401+awszztt@users.noreply.github.com>
2020-11-10 22:16:37 -0800

    add configurable time out (#635)
    * add configurable time out
    
    * add configurationValidationPolicy to the DeploymentDocumentation
    
    * fix the failure in the integration test
    
    * fix the benchMark
    
    * add import for com.amazonaws.services.evergreen.model.ConfigurationValidationPolicy
    
    * resolve the conflict with master
    
    * check whether getConfigurationValidationPolicy.getTimeout is null
    
    * rollback the change on the deploymentDocument json file
    
    * resovle conflict
    
    * reuse parameter
    
    Co-authored-by: Shagupta Shaikh <58999292+shaguptashaikh@users.noreply.github.com>

--- src/test/java/com/aws/greengrass/deployment/converter/DeploymentDocumentConverterTest.java
+++ src/test/java/com/aws/greengrass/deployment/converter/DeploymentDocumentConverterTest.java
@@ -14,6 +14,7 @@ import com.amazon.aws.iot.greengrass.configuration.common.Configuration;
 import com.amazonaws.arn.Arn;
 import com.amazonaws.services.evergreen.model.ComponentUpdatePolicy;
 import com.amazonaws.services.evergreen.model.ComponentUpdatePolicyAction;
+import com.amazonaws.services.evergreen.model.ConfigurationValidationPolicy;
 import com.aws.greengrass.deployment.model.ConfigurationUpdateOperation;
 import com.aws.greengrass.deployment.model.DeploymentDocument;
 import com.aws.greengrass.deployment.model.DeploymentPackageConfiguration;
@@ -218,6 +219,7 @@ class DeploymentDocumentConverterTest {
                     put("pkgA", new PackageInfo(true, "1.0.0", configMapA));
                     put("pkgB", new PackageInfo(false, "1.1.0", configMapB));
                 }}).componentUpdatePolicy(new ComponentUpdatePolicy().withAction("NOTIFY_COMPONENTS").withTimeout(60))
+                        .configurationValidationPolicy(new ConfigurationValidationPolicy().withTimeout(20))
                         .configurationArn(configurationArn).build();
 
         DeploymentDocument doc = DeploymentDocumentConverter.convertFromFleetConfiguration(config);
@@ -249,6 +251,7 @@ class DeploymentDocumentConverterTest {
                     put("pkgA", new PackageInfo(true, "1.0.0", configMapA));
                     put("pkgB", new PackageInfo(false, "1.1.0", configMapB));
                 }}).componentUpdatePolicy(new ComponentUpdatePolicy().withAction("NOTIFY_COMPONENTS").withTimeout(60))
+                        .configurationValidationPolicy(new ConfigurationValidationPolicy().withTimeout(20))
                         .configurationArn(configurationArn).build();
 
         DeploymentDocument doc = DeploymentDocumentConverter.convertFromFleetConfiguration(config);
