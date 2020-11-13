54e07e03e570b01e138b18a4acd20f9865e0d0a4
Tilo Chen <69258401+tilo-chen@users.noreply.github.com>
2020-11-12 20:42:38 -0500

    Shadow deployment report state (#574)
    * draft shadow deployment report state
    
    * add some fields
    
    * finish impl
    
    * fixes
    
    * add null check
    
    * address comments
    
    * report shadow cancel status; name change
    
    * name change
    
    * handle shadow update properly
    
    * update e2e test
    
    * update e2e test
    
    * add unit test assertions
    
    * add status details assertions
    
    Co-authored-by: fahadmohammed01 <52720275+fahadmohammed01@users.noreply.github.com>

--- src/integrationtests/java/com/aws/greengrass/integrationtests/e2e/deployment/DeploymentE2ETest.java
+++ src/integrationtests/java/com/aws/greengrass/integrationtests/e2e/deployment/DeploymentE2ETest.java
@@ -31,6 +31,7 @@ import com.aws.greengrass.mqttclient.MqttClient;
 import com.aws.greengrass.mqttclient.WrapperMqttClientConnection;
 import com.aws.greengrass.testcommons.testutilities.GGExtension;
 import com.aws.greengrass.testcommons.testutilities.TestUtils;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.hamcrest.collection.IsCollectionWithSize;
@@ -50,10 +51,9 @@ import software.amazon.awssdk.aws.greengrass.model.SubscribeToComponentUpdatesRe
 import software.amazon.awssdk.crt.mqtt.QualityOfService;
 import software.amazon.awssdk.eventstreamrpc.EventStreamRPCConnection;
 import software.amazon.awssdk.eventstreamrpc.StreamResponseHandler;
+import software.amazon.awssdk.iot.iotjobs.model.JobStatus;
 import software.amazon.awssdk.iot.iotshadow.IotShadowClient;
-import software.amazon.awssdk.iot.iotshadow.model.GetShadowRequest;
-import software.amazon.awssdk.iot.iotshadow.model.GetShadowSubscriptionRequest;
-import software.amazon.awssdk.iot.iotshadow.model.UpdateShadowSubscriptionRequest;
+import software.amazon.awssdk.iot.iotshadow.model.UpdateNamedShadowSubscriptionRequest;
 import software.amazon.awssdk.services.iot.model.DescribeJobExecutionRequest;
 import software.amazon.awssdk.services.iot.model.JobExecutionStatus;
 
@@ -69,6 +69,8 @@ import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Consumer;
 
+import static com.aws.greengrass.deployment.DeploymentService.DEPLOYMENT_DETAILED_STATUS_KEY;
+import static com.aws.greengrass.deployment.ShadowDeploymentListener.DEPLOYMENT_SHADOW_NAME;
 import static com.aws.greengrass.lifecyclemanager.GreengrassService.SERVICE_LIFECYCLE_NAMESPACE_TOPIC;
 import static com.aws.greengrass.testcommons.testutilities.ExceptionLogProtector.ignoreExceptionUltimateCauseOfType;
 import static com.aws.greengrass.testcommons.testutilities.ExceptionLogProtector.ignoreExceptionUltimateCauseWithMessage;
@@ -87,6 +89,7 @@ import static org.junit.jupiter.api.Assertions.fail;
 @ExtendWith(GGExtension.class)
 @Tag("E2E")
 class DeploymentE2ETest extends BaseE2ETestCase {
+    public static final String STATUS_KEY = "status";
     private CountDownLatch stdoutCountdown;
 
 
@@ -134,37 +137,39 @@ class DeploymentE2ETest extends BaseE2ETestCase {
         draftAndCreateDeployment(createDeploymentRequest);
         assertThat(kernel.getMain()::getState, eventuallyEval(is(State.FINISHED)));
 
-        assertTrue(cdlDeploymentFinished.await(5, TimeUnit.MINUTES));
-
-        Slf4jLogAdapter.removeGlobalListener(listener);
-        assertThat(getCloudDeployedComponent("CustomerApp")::getState, eventuallyEval(is(State.FINISHED)));
-        assertThat(getCloudDeployedComponent("SomeService")::getState, eventuallyEval(is(State.FINISHED)));
-
         IotShadowClient shadowClient = new IotShadowClient(
                 new WrapperMqttClientConnection(kernel.getContext().get(MqttClient.class)));
 
-        CountDownLatch getShadowCDL = new CountDownLatch(1);
-        GetShadowSubscriptionRequest request = new GetShadowSubscriptionRequest();
-        request.thingName = thingInfo.getThingName();
-        shadowClient.SubscribeToGetShadowAccepted(request, QualityOfService.AT_LEAST_ONCE, (response) -> {
-            //verify desired and reported state are same
-            assertEquals(response.state.desired, response.state.reported);
-            getShadowCDL.countDown();
-        });
-
-        UpdateShadowSubscriptionRequest req = new UpdateShadowSubscriptionRequest();
+        UpdateNamedShadowSubscriptionRequest req = new UpdateNamedShadowSubscriptionRequest();
+        req.shadowName = DEPLOYMENT_SHADOW_NAME;
         req.thingName = thingInfo.getThingName();
-        CountDownLatch updateShadowCDL = new CountDownLatch(1);
-        shadowClient.SubscribeToUpdateShadowAccepted(req, QualityOfService.AT_LEAST_ONCE , (response) -> {
-            updateShadowCDL.countDown();
+        CountDownLatch reportInProgressCdl = new CountDownLatch(1);
+        CountDownLatch reportSucceededCdl = new CountDownLatch(1);
+        shadowClient.SubscribeToUpdateNamedShadowAccepted(req, QualityOfService.AT_LEAST_ONCE , (response) -> {
+            try {
+                logger.info("Got shadow update: {}", new ObjectMapper().writeValueAsString(response));
+            } catch (JsonProcessingException e) {
+                // ignore
+            }
+            if (response.state.reported == null) {
+                return;
+            }
+            String reportedStatus = (String) response.state.reported.get(STATUS_KEY);
+            if (JobStatus.IN_PROGRESS.toString().equals(reportedStatus)) {
+                reportInProgressCdl.countDown();
+            } else if (JobStatus.SUCCEEDED.toString().equals(reportedStatus)) {
+                reportSucceededCdl.countDown();
+            }
         });
         // wait for the shadow's reported section to be updated
-        updateShadowCDL.await(30, TimeUnit.SECONDS);
-        GetShadowRequest request1 = new GetShadowRequest();
-        request1.thingName = thingInfo.getThingName();
-        //get shadow to verify that desired and reported section are in sync
-        shadowClient.PublishGetShadow(request1, QualityOfService.AT_LEAST_ONCE).get();
-        getShadowCDL.await(30, TimeUnit.SECONDS);
+        assertTrue(reportInProgressCdl.await(30, TimeUnit.SECONDS));
+        assertTrue(reportSucceededCdl.await(30, TimeUnit.SECONDS));
+
+        // deployment should succeed
+        assertTrue(cdlDeploymentFinished.await(30, TimeUnit.SECONDS));
+        Slf4jLogAdapter.removeGlobalListener(listener);
+        assertThat(getCloudDeployedComponent("CustomerApp")::getState, eventuallyEval(is(State.FINISHED)));
+        assertThat(getCloudDeployedComponent("SomeService")::getState, eventuallyEval(is(State.FINISHED)));
     }
 
 
@@ -453,7 +458,7 @@ class DeploymentE2ETest extends BaseE2ETestCase {
         // IoT Job should have failed with correct message.
         assertEquals(DeploymentResult.DeploymentStatus.FAILED_ROLLBACK_COMPLETE.name(), iotClient.describeJobExecution(
                 DescribeJobExecutionRequest.builder().jobId(jobId2).thingName(thingInfo.getThingName()).build())
-                .execution().statusDetails().detailsMap().get("detailed-deployment-status"));
+                .execution().statusDetails().detailsMap().get(DEPLOYMENT_DETAILED_STATUS_KEY));
     }
 
     @Timeout(value = 10, unit = TimeUnit.MINUTES)
