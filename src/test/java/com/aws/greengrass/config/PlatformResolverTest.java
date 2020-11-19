1cd556ea1229ac44c613c99752e2e6a02f52e241
Xueli Zheng <52835750+ShirleyZheng92@users.noreply.github.com>
2020-11-19 10:39:24 -0800

    Add more unit test for new platform resolving logic (#702)
    Co-authored-by: Xueli Zheng <shirlez@amazon.com>

--- src/test/java/com/aws/greengrass/config/PlatformResolverTest.java
+++ src/test/java/com/aws/greengrass/config/PlatformResolverTest.java
@@ -15,9 +15,10 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
@@ -64,23 +65,64 @@ class PlatformResolverTest {
     }
 
     @Test
-    void WHEN_config_contains_nulls_THEN_nulls_handled_sanely() {
-        Map<String, Object> nested = new HashMap<>();
-        Map<String, Object> data = new HashMap<>();
-        data.put("a", null);
-        data.put("b", nested);
-        nested.put("c", null);
-        nested.put("d", "e");
+    void WHEN_multi_platform_config_THEN_resolve_successfully() throws Exception {
+        String multiPlatformConfigString = "---\n"
+                + "key1: \n"
+                + "  darwin: darwinVal1\n"
+                + "  linux: linuxVal1\n"
+                + "  unix: unixVal1\n"
+                + "  windows: windowsVal1\n"
+                + "key2: \n"
+                + "  linux: linuxVal2\n"
+                + "  unix: unixVal2\n"
+                + "  windows: windowsVal2\n"
+                + "key3: \n"
+                + "  windows: windowsVal3\n"
+                + "  all: allVal3\n"
+                + "key4: \n"
+                + "  windows: \n"
+                + "    subkey4: windowsVal4\n"
+                + "key5: \n"
+                + "  windows: \n"
+                + "    subkey5: windowsVal5\n"
+                + "  subkey5: ignoredVal5\n";
+        Map<String, Object> multiPlatformConfig = MAPPER.readValue(multiPlatformConfigString, Map.class);
 
-        assertThat(PlatformResolver.filterPlatform(nested, new HashSet<String>(Arrays.asList("c", "d")),
-                Arrays.asList("c")).orElse("none"), equalTo("none"));
-        assertThat(PlatformResolver.filterPlatform(nested, new HashSet<String>(Arrays.asList("c", "d")),
-                Arrays.asList("d")).orElse("bad"), equalTo("e"));
-        Object o = PlatformResolver.filterPlatform(data, new HashSet<String>(Arrays.asList("a", "b")),
-                Arrays.asList("b")).orElse("bad");
+        Set<String> keywords = new HashSet<>(Arrays.asList("darwin", "linux", "unix", "windows", "all"));
+        List<String> selectors = Arrays.asList("darwin", "unix", "all");
+
+        String expectedResolvedConfigString = "---\n"
+                + "key1: darwinVal1\n"
+                + "key2: unixVal2\n"
+                + "key3: allVal3\n";
+
+        Map<String, Object> expectedResolvedConfig = MAPPER.readValue(expectedResolvedConfigString, Map.class);
+
+        assertThat(PlatformResolver.filterPlatform(multiPlatformConfig, keywords, selectors).get(),
+                equalTo(expectedResolvedConfig));
+    }
+
+
+    @Test
+    void WHEN_config_contains_nulls_THEN_nulls_handled_sanely() throws Exception {
+        String dataString = "---\n"
+                + "key1: null\n"
+                + "key2: \n"
+                + "  subKey3:  null\n"
+                + "  subKey4:  val4\n";
+
+        Map<String, Object> data = MAPPER.readValue(dataString, Map.class);
+        Map<String, Object> nested = (Map<String, Object>) data.get("key2");
+
+        assertThat(PlatformResolver.filterPlatform(nested, new HashSet<>(Arrays.asList("subKey3", "subKey4")),
+                Arrays.asList("subKey3")).orElse("none"), equalTo("none"));
+        assertThat(PlatformResolver.filterPlatform(nested, new HashSet<>(Arrays.asList("subKey3", "subKey4")),
+                Arrays.asList("subKey4")).orElse("bad"), equalTo("val4"));
+        Object o = PlatformResolver.filterPlatform(data, new HashSet<String>(Arrays.asList("key1", "key2")),
+                Arrays.asList("key2")).orElse("bad");
         assertThat(o, isA(Map.class));
         Map<String,Object> m = (Map<String,Object>)o;
         assertThat(m.size(), equalTo(1));
-        assertThat(m.get("d"), equalTo("e"));
+        assertThat(m.get("subKey4"), equalTo("val4"));
     }
 }
