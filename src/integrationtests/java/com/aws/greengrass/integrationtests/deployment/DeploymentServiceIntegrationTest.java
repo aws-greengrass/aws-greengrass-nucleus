d36084dcc7c88bc85e6367e9922009d1878b5396
abanthiy <58749232+abanthiy@users.noreply.github.com>
2020-11-25 16:51:02 -0800

    Remove taking configuration snapshot after deployment is finished (#721)
    * Taking configuration snapshot after deployment is finished
    
    * Revert changing snapshot
    
    Co-authored-by: Hui Yang <hui-yang@users.noreply.github.com>
    Co-authored-by: Hui Yang <huiyn@amazon.com>

--- src/integrationtests/java/com/aws/greengrass/integrationtests/deployment/DeploymentServiceIntegrationTest.java
+++ src/integrationtests/java/com/aws/greengrass/integrationtests/deployment/DeploymentServiceIntegrationTest.java
@@ -107,7 +107,7 @@ public class DeploymentServiceIntegrationTest extends BaseITCase {
     }
 
     @Test
-    void GIVEN_device_deployment_not_started_WHEN_new_deployment_THEN_first_deployment_cancelled(ExtensionContext context) throws Exception {
+    void GIVEN_device_deployment_not_started_WHEN_new_deployment_THEN_first_deployment_cancelled() throws Exception {
         CountDownLatch cdlDeployNonDisruptable = new CountDownLatch(1);
         CountDownLatch cdlDeployRedSignal = new CountDownLatch(1);
         CountDownLatch cdlRedeployNonDisruptable = new CountDownLatch(1);
