d45e54831081037edce6c75efe342481d60e1526
Francis Fu <fufranci@amazon.com>
2021-04-20 13:50:37 -0700

    fix(windows): remove unused import!

--- src/main/java/com/aws/greengrass/util/platforms/windows/WindowsPlatform.java
+++ src/main/java/com/aws/greengrass/util/platforms/windows/WindowsPlatform.java
@@ -44,7 +44,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
-import java.util.UUID;
 
 public class WindowsPlatform extends Platform {
     private static final String NAMED_PIPE = "\\\\.\\pipe\\NucleusNamedPipe-";
