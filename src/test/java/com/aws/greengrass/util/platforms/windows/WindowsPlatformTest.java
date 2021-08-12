cb8cb77e0ae2a5e460cae4aadf181a636bacafcd
Francis Fu <fufranci@amazon.com>
2021-08-12 09:14:30 -0700

    WIP

--- src/test/java/com/aws/greengrass/util/platforms/windows/WindowsPlatformTest.java
+++ src/test/java/com/aws/greengrass/util/platforms/windows/WindowsPlatformTest.java
@@ -7,12 +7,21 @@ package com.aws.greengrass.util.platforms.windows;
 
 import com.aws.greengrass.testcommons.testutilities.GGExtension;
 import com.aws.greengrass.util.FileSystemPermission;
+import com.sun.jna.platform.win32.AccCtrl;
+import com.sun.jna.platform.win32.Advapi32;
+import com.sun.jna.platform.win32.Kernel32;
+import com.sun.jna.platform.win32.Kernel32Util;
+import com.sun.jna.platform.win32.WinBase;
+import com.sun.jna.platform.win32.WinNT;
+import com.sun.jna.ptr.PointerByReference;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.EnabledOnOs;
 import org.junit.jupiter.api.condition.OS;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.io.TempDir;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.LinkOption;
@@ -28,6 +37,17 @@ import java.util.Collections;
 import java.util.List;
 import java.util.UUID;
 
+import static com.sun.jna.platform.win32.AccCtrl.SE_OBJECT_TYPE.SE_KERNEL_OBJECT;
+import static com.sun.jna.platform.win32.WinBase.PIPE_ACCEPT_REMOTE_CLIENTS;
+import static com.sun.jna.platform.win32.WinBase.PIPE_ACCESS_DUPLEX;
+import static com.sun.jna.platform.win32.WinBase.PIPE_READMODE_BYTE;
+import static com.sun.jna.platform.win32.WinBase.PIPE_TYPE_BYTE;
+import static com.sun.jna.platform.win32.WinBase.PIPE_UNLIMITED_INSTANCES;
+import static com.sun.jna.platform.win32.WinBase.PIPE_WAIT;
+import static com.sun.jna.platform.win32.WinNT.DACL_SECURITY_INFORMATION;
+import static com.sun.jna.platform.win32.WinNT.FILE_FLAG_OVERLAPPED;
+import static com.sun.jna.platform.win32.WinNT.GROUP_SECURITY_INFORMATION;
+import static com.sun.jna.platform.win32.WinNT.OWNER_SECURITY_INFORMATION;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.arrayContaining;
 import static org.hamcrest.Matchers.containsInAnyOrder;
@@ -39,6 +59,8 @@ import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.lessThanOrEqualTo;
 import static org.hamcrest.Matchers.not;
 import static org.hamcrest.text.MatchesPattern.matchesPattern;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ExtendWith({GGExtension.class})
 @EnabledOnOs(OS.WINDOWS)
@@ -220,5 +242,145 @@ class WindowsPlatformTest {
         WindowsPlatform windowsPlatform = new WindowsPlatform();
         WindowsGroupAttributes windowsGroupAttributes = windowsPlatform.lookupGroupByIdentifier(EVERYONE_SID);
         assertThat(windowsGroupAttributes.getPrincipalName(), equalTo(EVERYONE));
+    }
+
+    @Test
+    void GIVEN_a_named_pipe_WHEN_setIpcFilePermissions_THEN_named_pipe_is_public_accessible() {
+        WindowsPlatform windowsPlatform = new WindowsPlatform();
+
+        // Create a named pipe, passing null as lpSecurityAttributes (sane as Device SDK)
+        String namedPipe = windowsPlatform.prepareIpcFilepath(tempDir);
+        WinNT.HANDLE handle = Kernel32.INSTANCE.CreateNamedPipe(namedPipe,  // lpName
+                PIPE_ACCESS_DUPLEX | FILE_FLAG_OVERLAPPED,  // dwOpenMode
+                PIPE_TYPE_BYTE | PIPE_READMODE_BYTE | PIPE_WAIT | PIPE_ACCEPT_REMOTE_CLIENTS, // dwPipeMode
+                PIPE_UNLIMITED_INSTANCES, // nMaxInstances
+                512, // nOutBufferSize
+                512, // nInBufferSize
+                0, // nDefaultTimeOut
+                null); // lpSecurityAttributes
+        assertFalse(WinBase.INVALID_HANDLE_VALUE.equals(handle));
+
+        // Test access right
+        int infoType = OWNER_SECURITY_INFORMATION
+                | GROUP_SECURITY_INFORMATION
+                | DACL_SECURITY_INFORMATION;
+
+        PointerByReference ppsidOwner = new PointerByReference();
+        PointerByReference ppsidGroup = new PointerByReference();
+        PointerByReference ppDacl = new PointerByReference();
+        PointerByReference ppSecurityDescriptor = new PointerByReference();
+
+        int ret = Advapi32.INSTANCE.GetSecurityInfo(handle,
+                SE_KERNEL_OBJECT,
+                infoType,
+                ppsidOwner,
+                ppsidGroup,
+                ppDacl,
+                null,
+                ppSecurityDescriptor);
+        assertEquals(0, ret);
+
+        // Change access right
+
+        // This code should be implemented in WindowsPlatform.setIpcFilePermissions.
+        // windowsPlatform.setIpcFilePermissions(tempDir);
+        ret = Advapi32.INSTANCE.SetSecurityInfo(handle,
+                SE_KERNEL_OBJECT,
+                infoType,
+                ppsidOwner.getValue(),
+                ppsidGroup.getValue(),
+
+                // https://docs.microsoft.com/en-us/windows/win32/secauthz/access-control-lists
+                // "If the object does not have a DACL, the system grants full access to everyone."
+                ppDacl.getValue(),
+
+                null);
+        assertEquals(0, ret); // ERROR_ACCESS_DENIED
+
+        // Test access right again
+        ppsidOwner = new PointerByReference();
+        ppsidGroup = new PointerByReference();
+        ppDacl = new PointerByReference();
+        ppSecurityDescriptor = new PointerByReference();
+
+        ret = Advapi32.INSTANCE.GetSecurityInfo(handle,
+                SE_KERNEL_OBJECT,
+                infoType,
+                ppsidOwner,
+                ppsidGroup,
+                ppDacl,
+                null,
+                ppSecurityDescriptor);
+        assertEquals(0, ret);
+    }
+
+    // Copied from: https://github.com/java-native-access/jna/blob/master/contrib/platform/test/com/sun/jna/platform/win32/Advapi32Test.java#L1319
+    @Test
+    public void testGetSetSecurityInfoNoSACL() throws Exception {
+        int infoType = OWNER_SECURITY_INFORMATION
+                | GROUP_SECURITY_INFORMATION
+                | DACL_SECURITY_INFORMATION;
+
+        PointerByReference ppsidOwner = new PointerByReference();
+        PointerByReference ppsidGroup = new PointerByReference();
+        PointerByReference ppDacl = new PointerByReference();
+        PointerByReference ppSecurityDescriptor = new PointerByReference();
+        // create a temp file
+        File file = createTempFile();
+        String filePath = file.getAbsolutePath();
+
+        WinNT.HANDLE hFile = Kernel32.INSTANCE.CreateFile(
+                filePath,
+                WinNT.GENERIC_ALL | WinNT.WRITE_OWNER | WinNT.WRITE_DAC,
+                WinNT.FILE_SHARE_READ,
+                new WinBase.SECURITY_ATTRIBUTES(),
+                WinNT.OPEN_EXISTING,
+                WinNT.FILE_ATTRIBUTE_NORMAL,
+                null);
+        assertFalse(WinBase.INVALID_HANDLE_VALUE.equals(hFile));
+
+        try {
+            try {
+
+                assertEquals(0,
+                        Advapi32.INSTANCE.GetSecurityInfo(
+                                hFile,
+                                AccCtrl.SE_OBJECT_TYPE.SE_FILE_OBJECT,
+                                infoType,
+                                ppsidOwner,
+                                ppsidGroup,
+                                ppDacl,
+                                null,
+                                ppSecurityDescriptor));
+
+                assertEquals(0,
+                        Advapi32.INSTANCE.SetSecurityInfo(
+                                hFile,
+                                AccCtrl.SE_OBJECT_TYPE.SE_FILE_OBJECT,
+                                infoType,
+                                ppsidOwner.getValue(),
+                                ppsidGroup.getValue(),
+                                ppDacl.getValue(),
+                                null));
+            } finally {
+                Kernel32.INSTANCE.CloseHandle(hFile);
+                file.delete();
+            }
+        } finally {
+            Kernel32Util.freeLocalMemory(ppSecurityDescriptor.getValue());
+        }
+    }
+
+    private File createTempFile() throws Exception {
+        String filePath = System.getProperty("java.io.tmpdir") + System.nanoTime()
+                + ".text";
+        File file = new File(filePath);
+        file.createNewFile();
+        FileWriter fileWriter = new FileWriter(file);
+        for (int i = 0; i < 1000; i++) {
+            fileWriter.write("Sample text " + i + System.getProperty("line.separator"));
+        }
+        fileWriter.close();
+        return file;
     }
 }
