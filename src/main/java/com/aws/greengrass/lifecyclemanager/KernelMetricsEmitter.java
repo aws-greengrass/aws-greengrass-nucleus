8127ef95823fef3e66573de16484a68494032d8c
Ashay Vipinkumar <68658347+avipinku@users.noreply.github.com>
2021-09-10 17:31:21 -0700

    feat: decouple metric collection from emission (#1070)

--- src/main/java/com/aws/greengrass/lifecyclemanager/KernelMetricsEmitter.java
+++ src/main/java/com/aws/greengrass/lifecyclemanager/KernelMetricsEmitter.java
@@ -14,8 +14,11 @@ import com.aws.greengrass.telemetry.impl.MetricFactory;
 import com.aws.greengrass.telemetry.models.TelemetryAggregation;
 import com.aws.greengrass.telemetry.models.TelemetryUnit;
 
+import java.time.Instant;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.EnumMap;
+import java.util.List;
 import java.util.Map;
 import javax.inject.Inject;
 
@@ -41,81 +44,116 @@ public class KernelMetricsEmitter extends PeriodicMetricsEmitter {
      */
     @Override
     public void emitMetrics() {
+        List<Metric> retrievedMetrics = getMetrics();
+        for (Metric retrievedMetric : retrievedMetrics) {
+            mf.putMetricData(retrievedMetric);
+        }
+    }
+
+    /**
+     * Retrieve kernel component state metrics.
+     * @return a list of {@link Metric}
+     */
+    @Override
+    public List<Metric> getMetrics() {
         Map<State, Integer> stateCount = new EnumMap<>(State.class);
         Collection<GreengrassService> services = kernel.orderedDependencies();
         for (GreengrassService service : services) {
             stateCount.put(service.getState(), stateCount.getOrDefault(service.getState(), 0) + 1);
         }
+
+        List<Metric> metricsList = new ArrayList<>();
+        long timestamp = Instant.now().toEpochMilli();
         Metric metric = Metric.builder()
                 .namespace(NAMESPACE)
                 .name("NumberOfComponentsStarting")
                 .unit(TelemetryUnit.Count)
                 .aggregation(TelemetryAggregation.Average)
+                .value(stateCount.getOrDefault(State.STARTING, 0))
+                .timestamp(timestamp)
                 .build();
-        mf.putMetricData(metric, stateCount.getOrDefault(State.STARTING, 0));
+        metricsList.add(metric);
 
         metric = Metric.builder()
                 .namespace(NAMESPACE)
                 .name("NumberOfComponentsInstalled")
                 .unit(TelemetryUnit.Count)
                 .aggregation(TelemetryAggregation.Average)
+                .value(stateCount.getOrDefault(State.INSTALLED, 0))
+                .timestamp(timestamp)
                 .build();
-        mf.putMetricData(metric, stateCount.getOrDefault(State.INSTALLED, 0));
+        metricsList.add(metric);
 
         metric = Metric.builder()
                 .namespace(NAMESPACE)
                 .name("NumberOfComponentsStateless")
                 .unit(TelemetryUnit.Count)
                 .aggregation(TelemetryAggregation.Average)
+                .value(stateCount.getOrDefault(State.STATELESS, 0))
+                .timestamp(timestamp)
                 .build();
-        mf.putMetricData(metric, stateCount.getOrDefault(State.STATELESS, 0));
+        metricsList.add(metric);
 
         metric = Metric.builder()
                 .namespace(NAMESPACE)
                 .name("NumberOfComponentsStopping")
                 .unit(TelemetryUnit.Count)
                 .aggregation(TelemetryAggregation.Average)
+                .value(stateCount.getOrDefault(State.STOPPING, 0))
+                .timestamp(timestamp)
                 .build();
-        mf.putMetricData(metric, stateCount.getOrDefault(State.STOPPING, 0));
+        metricsList.add(metric);
 
         metric = Metric.builder()
                 .namespace(NAMESPACE)
                 .name("NumberOfComponentsBroken")
                 .unit(TelemetryUnit.Count)
                 .aggregation(TelemetryAggregation.Average)
+                .value(stateCount.getOrDefault(State.BROKEN, 0))
+                .timestamp(timestamp)
                 .build();
-        mf.putMetricData(metric, stateCount.getOrDefault(State.BROKEN, 0));
+        metricsList.add(metric);
 
         metric = Metric.builder()
                 .namespace(NAMESPACE)
                 .name("NumberOfComponentsRunning")
                 .unit(TelemetryUnit.Count)
                 .aggregation(TelemetryAggregation.Average)
+                .value(stateCount.getOrDefault(State.RUNNING, 0))
+                .timestamp(timestamp)
                 .build();
-        mf.putMetricData(metric, stateCount.getOrDefault(State.RUNNING, 0));
+        metricsList.add(metric);
 
         metric = Metric.builder()
                 .namespace(NAMESPACE)
                 .name("NumberOfComponentsErrored")
                 .unit(TelemetryUnit.Count)
                 .aggregation(TelemetryAggregation.Average)
+                .value(stateCount.getOrDefault(State.ERRORED, 0))
+                .timestamp(timestamp)
                 .build();
-        mf.putMetricData(metric, stateCount.getOrDefault(State.ERRORED, 0));
+        metricsList.add(metric);
 
         metric = Metric.builder()
                 .namespace(NAMESPACE)
                 .name("NumberOfComponentsNew")
                 .unit(TelemetryUnit.Count)
                 .aggregation(TelemetryAggregation.Average)
+                .value(stateCount.getOrDefault(State.NEW, 0))
+                .timestamp(timestamp)
                 .build();
-        mf.putMetricData(metric, stateCount.getOrDefault(State.NEW, 0));
+        metricsList.add(metric);
 
         metric = Metric.builder()
                 .namespace(NAMESPACE)
                 .name("NumberOfComponentsFinished")
                 .unit(TelemetryUnit.Count)
                 .aggregation(TelemetryAggregation.Average)
+                .value(stateCount.getOrDefault(State.FINISHED, 0))
+                .timestamp(timestamp)
                 .build();
-        mf.putMetricData(metric, stateCount.getOrDefault(State.FINISHED, 0));
+        metricsList.add(metric);
+
+        return metricsList;
     }
 }
