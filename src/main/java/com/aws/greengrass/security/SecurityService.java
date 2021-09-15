832388d76f515f880b3d41ee4fce40758b253d81
Jason Wang <zhengang@amazon.com>
2021-09-14 17:02:13 -0700

    feat: implement default file based crypto key service provider

--- src/main/java/com/aws/greengrass/security/SecurityService.java
+++ src/main/java/com/aws/greengrass/security/SecurityService.java
@@ -11,25 +11,47 @@ import com.aws.greengrass.logging.impl.LogManager;
 import com.aws.greengrass.security.exceptions.KeyLoadingException;
 import com.aws.greengrass.security.exceptions.ServiceProviderConflictException;
 import com.aws.greengrass.security.exceptions.ServiceUnavailableException;
+import com.aws.greengrass.util.EncryptionUtils;
 import lombok.AccessLevel;
 import lombok.Getter;
 
+import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.security.GeneralSecurityException;
+import java.security.KeyStore;
+import java.security.PrivateKey;
+import java.security.cert.Certificate;
+import java.security.cert.X509Certificate;
+import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import javax.net.ssl.KeyManager;
+import javax.net.ssl.KeyManagerFactory;
 
-public class SecurityService {
+public final class SecurityService {
     private static final Logger logger = LogManager.getLogger(SecurityService.class);
     private static final String KEY_TYPE = "keyType";
     private static final String KEY_URI = "keyUri";
+    private static final String CERT_URI = "certificateUri";
 
     @Getter(AccessLevel.PACKAGE)
     private final ConcurrentMap<CaseInsensitiveString, CryptoKeySpi> cryptoKeyProviderMap;
 
+    /**
+     * Constructor of security service.
+     */
     public SecurityService() {
         this.cryptoKeyProviderMap = new ConcurrentHashMap<>();
+
+        // instantiate and register the default file based provider
+        CryptoKeySpi defaultProvider = new DefaultCryptoKeyProvider();
+        try {
+            this.registerCryptoKeyProvider(defaultProvider);
+        } catch (ServiceProviderConflictException e) {
+            // it won't happen, it's programming error if it does
+            throw new RuntimeException("Default crypto key provider has been registered", e);
+        }
     }
 
     /**
@@ -66,17 +88,19 @@ public class SecurityService {
     /**
      * Get JSSE KeyManagers, used for https TLS handshake.
      *
-     * @param keyUri private key URI
+     * @param privateKeyUri private key URI
+     * @param certificateUri certificate URI
      * @return KeyManagers that manage the specified private key
      * @throws ServiceUnavailableException if crypto key provider service is unavailable
      * @throws KeyLoadingException if crypto key provider service fails to load key
      * @throws URISyntaxException if key URI syntax error
      */
-    public KeyManager[] getKeyManagers(String keyUri)
+    public KeyManager[] getKeyManagers(String privateKeyUri, String certificateUri)
             throws ServiceUnavailableException, KeyLoadingException, URISyntaxException {
-        logger.atTrace().kv(KEY_URI, keyUri).log("Get key managers by key URI");
-        CryptoKeySpi provider = selectCryptoKeyProvider(keyUri);
-        return provider.getKeyManagers(keyUri);
+        logger.atTrace().kv(KEY_URI, privateKeyUri).kv(CERT_URI, certificateUri)
+                .log("Get key managers by key URI");
+        CryptoKeySpi provider = selectCryptoKeyProvider(privateKeyUri);
+        return provider.getKeyManagers(privateKeyUri, certificateUri);
     }
 
     private CryptoKeySpi selectCryptoKeyProvider(String keyUri) throws URISyntaxException, ServiceUnavailableException {
@@ -88,5 +112,52 @@ public class SecurityService {
             throw new ServiceUnavailableException(String.format("Crypto key service for %s is unavailable", keyType));
         }
         return provider;
+    }
+
+    static class DefaultCryptoKeyProvider implements CryptoKeySpi {
+        private static final Logger logger = LogManager.getLogger(DefaultCryptoKeyProvider.class);
+
+        @Override
+        public KeyManager[] getKeyManagers(String privateKeyUriStr, String certificateUriStr)
+                throws KeyLoadingException, URISyntaxException {
+            URI privateKeyUri = new URI(privateKeyUriStr);
+            if (!isUriSupportedKeyType(privateKeyUri)) {
+                logger.atError().kv(KEY_URI, privateKeyUriStr).log("Can't process the key type");
+                throw new KeyLoadingException(String.format("Only support %s type private key", supportedKeyType()));
+            }
+
+            URI certificateUri = new URI(certificateUriStr);
+            if (!isUriSupportedKeyType(certificateUri)) {
+                logger.atError().kv(CERT_URI, certificateUriStr).log("Can't process the certificate type");
+                throw new KeyLoadingException(String.format("Only support %s type certificate", supportedKeyType()));
+            }
+
+            try {
+                PrivateKey privateKey = EncryptionUtils.loadPrivateKey(privateKeyUri.getPath());
+                List<X509Certificate> certificateChain = EncryptionUtils.loadX509Certificates(certificateUri.getPath());
+
+                KeyStore keyStore = KeyStore.getInstance("PKCS12");
+                keyStore.load(null);
+                keyStore.setKeyEntry("private-key", privateKey, null, certificateChain.toArray(new Certificate[0]));
+
+                KeyManagerFactory keyManagerFactory =
+                        KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+                keyManagerFactory.init(keyStore, null);
+                return keyManagerFactory.getKeyManagers();
+            } catch (GeneralSecurityException | IOException e) {
+                logger.atError().kv(KEY_URI, privateKeyUriStr).kv(CERT_URI, certificateUriStr)
+                        .log("Exception caught during getting key manager");
+                throw new KeyLoadingException("Failed to get key manager", e);
+            }
+        }
+
+        @Override
+        public String supportedKeyType() {
+            return "file";
+        }
+
+        private boolean isUriSupportedKeyType(URI uri) {
+            return new CaseInsensitiveString(supportedKeyType()).equals(new CaseInsensitiveString(uri.getScheme()));
+        }
     }
 }
