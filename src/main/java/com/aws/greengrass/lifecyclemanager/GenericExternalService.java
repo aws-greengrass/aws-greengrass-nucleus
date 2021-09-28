bce916ae2d2d7e21b993718a1c519163e0015ce1
Tilo Chen <tiangac@amazon.com>
2021-09-28 10:19:00 -0700

    fix: if condition

--- src/main/java/com/aws/greengrass/lifecyclemanager/GenericExternalService.java
+++ src/main/java/com/aws/greengrass/lifecyclemanager/GenericExternalService.java
@@ -122,7 +122,7 @@ public class GenericExternalService extends GreengrassService {
 
             // Reinstall for changes to the install script or if the package version changed, or runWith user changed
             if (child.childOf(Lifecycle.LIFECYCLE_INSTALL_NAMESPACE_TOPIC) || child.childOf(VERSION_CONFIG_KEY)
-                    || child.childOf(POSIX_USER_KEY) || child.childOf(WINDOWS_USER_KEY)) {
+                    || child.childOf(RUN_WITH_NAMESPACE_TOPIC) && !child.childOf(SYSTEM_RESOURCE_LIMITS_TOPICS)) {
                 logger.atInfo("service-config-change").kv("configNode", child.getFullName())
                         .log("Requesting reinstallation for component");
                 requestReinstall();
