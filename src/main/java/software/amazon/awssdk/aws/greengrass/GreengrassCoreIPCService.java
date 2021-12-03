beb4e513ef928ec7fbbbea94bd9dd99a001df87f
philcali <philip.cali@gmail.com>
2021-12-03 15:11:23 -0500

    chore: more cluster operations

--- src/main/java/software/amazon/awssdk/aws/greengrass/GreengrassCoreIPCService.java
+++ src/main/java/software/amazon/awssdk/aws/greengrass/GreengrassCoreIPCService.java
@@ -32,6 +32,10 @@ public final class GreengrassCoreIPCService extends EventStreamRPCServiceHandler
 
   public static final String DELETE_THING_SHADOW = SERVICE_NAMESPACE + "#DeleteThingShadow";
 
+  public static final String RETRIEVE_SHARED_LOCK = SERVICE_NAMESPACE + "#RetrieveSharedLock";
+
+  public static final String EXTEND_SHARED_LOCK = SERVICE_NAMESPACE + "#ExtendSharedLock";
+
   public static final String DEFER_COMPONENT_UPDATE = SERVICE_NAMESPACE + "#DeferComponentUpdate";
 
   public static final String SUBSCRIBE_TO_VALIDATE_CONFIGURATION_UPDATES = SERVICE_NAMESPACE + "#SubscribeToValidateConfigurationUpdates";
@@ -42,7 +46,11 @@ public final class GreengrassCoreIPCService extends EventStreamRPCServiceHandler
 
   public static final String GET_COMPONENT_DETAILS = SERVICE_NAMESPACE + "#GetComponentDetails";
 
+  public static final String RETRIEVE_SHARED_PROPERTY = SERVICE_NAMESPACE + "#RetrieveSharedProperty";
+
   public static final String PUBLISH_TO_TOPIC = SERVICE_NAMESPACE + "#PublishToTopic";
+
+  public static final String CREATE_SHARED_LOCK = SERVICE_NAMESPACE + "#CreateSharedLock";
 
   public static final String LIST_COMPONENTS = SERVICE_NAMESPACE + "#ListComponents";
 
@@ -68,9 +76,17 @@ public final class GreengrassCoreIPCService extends EventStreamRPCServiceHandler
 
   public static final String GET_SECRET_VALUE = SERVICE_NAMESPACE + "#GetSecretValue";
 
+  public static final String QUERY_SHARED_PROPERTIES = SERVICE_NAMESPACE + "#QuerySharedProperties";
+
+  public static final String UNLOCK_SHARED_PROPERTY = SERVICE_NAMESPACE + "#UnlockSharedProperty";
+
   public static final String UPDATE_STATE = SERVICE_NAMESPACE + "#UpdateState";
 
+  public static final String PUBLISH_SHARED_PROPERTY = SERVICE_NAMESPACE + "#PublishSharedProperty";
+
   public static final String LIST_NAMED_SHADOWS_FOR_THING = SERVICE_NAMESPACE + "#ListNamedShadowsForThing";
+
+  public static final String DELETE_SHARED_PROPERTY = SERVICE_NAMESPACE + "#DeleteSharedProperty";
 
   public static final String SUBSCRIBE_TO_COMPONENT_UPDATES = SERVICE_NAMESPACE + "#SubscribeToComponentUpdates";
 
@@ -89,12 +105,16 @@ public final class GreengrassCoreIPCService extends EventStreamRPCServiceHandler
     SERVICE_OPERATION_SET.add(PUBLISH_TO_IOT_CORE);
     SERVICE_OPERATION_SET.add(SUBSCRIBE_TO_CONFIGURATION_UPDATE);
     SERVICE_OPERATION_SET.add(DELETE_THING_SHADOW);
+    SERVICE_OPERATION_SET.add(RETRIEVE_SHARED_LOCK);
+    SERVICE_OPERATION_SET.add(EXTEND_SHARED_LOCK);
     SERVICE_OPERATION_SET.add(DEFER_COMPONENT_UPDATE);
     SERVICE_OPERATION_SET.add(SUBSCRIBE_TO_VALIDATE_CONFIGURATION_UPDATES);
     SERVICE_OPERATION_SET.add(GET_CONFIGURATION);
     SERVICE_OPERATION_SET.add(SUBSCRIBE_TO_TOPIC);
     SERVICE_OPERATION_SET.add(GET_COMPONENT_DETAILS);
+    SERVICE_OPERATION_SET.add(RETRIEVE_SHARED_PROPERTY);
     SERVICE_OPERATION_SET.add(PUBLISH_TO_TOPIC);
+    SERVICE_OPERATION_SET.add(CREATE_SHARED_LOCK);
     SERVICE_OPERATION_SET.add(LIST_COMPONENTS);
     SERVICE_OPERATION_SET.add(CREATE_DEBUG_PASSWORD);
     SERVICE_OPERATION_SET.add(GET_THING_SHADOW);
@@ -107,8 +127,12 @@ public final class GreengrassCoreIPCService extends EventStreamRPCServiceHandler
     SERVICE_OPERATION_SET.add(RESTART_COMPONENT);
     SERVICE_OPERATION_SET.add(GET_LOCAL_DEPLOYMENT_STATUS);
     SERVICE_OPERATION_SET.add(GET_SECRET_VALUE);
+    SERVICE_OPERATION_SET.add(QUERY_SHARED_PROPERTIES);
+    SERVICE_OPERATION_SET.add(UNLOCK_SHARED_PROPERTY);
     SERVICE_OPERATION_SET.add(UPDATE_STATE);
+    SERVICE_OPERATION_SET.add(PUBLISH_SHARED_PROPERTY);
     SERVICE_OPERATION_SET.add(LIST_NAMED_SHADOWS_FOR_THING);
+    SERVICE_OPERATION_SET.add(DELETE_SHARED_PROPERTY);
     SERVICE_OPERATION_SET.add(SUBSCRIBE_TO_COMPONENT_UPDATES);
     SERVICE_OPERATION_SET.add(LIST_LOCAL_DEPLOYMENTS);
     SERVICE_OPERATION_SET.add(STOP_COMPONENT);
@@ -152,6 +176,16 @@ public final class GreengrassCoreIPCService extends EventStreamRPCServiceHandler
     operationSupplierMap.put(DELETE_THING_SHADOW, handler);
   }
 
+  public void setRetrieveSharedLockHandler(
+      Function<OperationContinuationHandlerContext, GeneratedAbstractRetrieveSharedLockOperationHandler> handler) {
+    operationSupplierMap.put(RETRIEVE_SHARED_LOCK, handler);
+  }
+
+  public void setExtendSharedLockHandler(
+      Function<OperationContinuationHandlerContext, GeneratedAbstractExtendSharedLockOperationHandler> handler) {
+    operationSupplierMap.put(EXTEND_SHARED_LOCK, handler);
+  }
+
   public void setDeferComponentUpdateHandler(
       Function<OperationContinuationHandlerContext, GeneratedAbstractDeferComponentUpdateOperationHandler> handler) {
     operationSupplierMap.put(DEFER_COMPONENT_UPDATE, handler);
@@ -177,9 +211,19 @@ public final class GreengrassCoreIPCService extends EventStreamRPCServiceHandler
     operationSupplierMap.put(GET_COMPONENT_DETAILS, handler);
   }
 
+  public void setRetrieveSharedPropertyHandler(
+      Function<OperationContinuationHandlerContext, GeneratedAbstractRetrieveSharedPropertyOperationHandler> handler) {
+    operationSupplierMap.put(RETRIEVE_SHARED_PROPERTY, handler);
+  }
+
   public void setPublishToTopicHandler(
       Function<OperationContinuationHandlerContext, GeneratedAbstractPublishToTopicOperationHandler> handler) {
     operationSupplierMap.put(PUBLISH_TO_TOPIC, handler);
+  }
+
+  public void setCreateSharedLockHandler(
+      Function<OperationContinuationHandlerContext, GeneratedAbstractCreateSharedLockOperationHandler> handler) {
+    operationSupplierMap.put(CREATE_SHARED_LOCK, handler);
   }
 
   public void setListComponentsHandler(
@@ -242,14 +286,34 @@ public final class GreengrassCoreIPCService extends EventStreamRPCServiceHandler
     operationSupplierMap.put(GET_SECRET_VALUE, handler);
   }
 
+  public void setQuerySharedPropertiesHandler(
+      Function<OperationContinuationHandlerContext, GeneratedAbstractQuerySharedPropertiesOperationHandler> handler) {
+    operationSupplierMap.put(QUERY_SHARED_PROPERTIES, handler);
+  }
+
+  public void setUnlockSharedPropertyHandler(
+      Function<OperationContinuationHandlerContext, GeneratedAbstractUnlockSharedPropertyOperationHandler> handler) {
+    operationSupplierMap.put(UNLOCK_SHARED_PROPERTY, handler);
+  }
+
   public void setUpdateStateHandler(
       Function<OperationContinuationHandlerContext, GeneratedAbstractUpdateStateOperationHandler> handler) {
     operationSupplierMap.put(UPDATE_STATE, handler);
   }
 
+  public void setPublishSharedPropertyHandler(
+      Function<OperationContinuationHandlerContext, GeneratedAbstractPublishSharedPropertyOperationHandler> handler) {
+    operationSupplierMap.put(PUBLISH_SHARED_PROPERTY, handler);
+  }
+
   public void setListNamedShadowsForThingHandler(
       Function<OperationContinuationHandlerContext, GeneratedAbstractListNamedShadowsForThingOperationHandler> handler) {
     operationSupplierMap.put(LIST_NAMED_SHADOWS_FOR_THING, handler);
+  }
+
+  public void setDeleteSharedPropertyHandler(
+      Function<OperationContinuationHandlerContext, GeneratedAbstractDeleteSharedPropertyOperationHandler> handler) {
+    operationSupplierMap.put(DELETE_SHARED_PROPERTY, handler);
   }
 
   public void setSubscribeToComponentUpdatesHandler(
