ca3ddd873cea9b325132297be57d855181844649
Joseph Martin <62030418+jmrtinb@users.noreply.github.com>
2021-12-06 15:25:56 -0500

    feat: add optional setting to enable interpolation in configuration during deployments (#1134)

--- src/main/java/com/aws/greengrass/deployment/DeviceConfiguration.java
+++ src/main/java/com/aws/greengrass/deployment/DeviceConfiguration.java
@@ -99,6 +99,7 @@ public class DeviceConfiguration {
     public static final String DEVICE_PARAM_PRIVATE_KEY_PATH = "privateKeyPath";
     public static final String DEVICE_PARAM_CERTIFICATE_FILE_PATH = "certificateFilePath";
     public static final String DEVICE_PARAM_ROOT_CA_PATH = "rootCaPath";
+    public static final String DEVICE_PARAM_INTERPOLATE_COMPONENT_CONFIGURATION = "interpolateComponentConfiguration";
     public static final String SYSTEM_NAMESPACE_KEY = "system";
     public static final String PLATFORM_OVERRIDE_TOPIC = "platformOverride";
     public static final String DEVICE_PARAM_AWS_REGION = "awsRegion";
@@ -583,6 +584,10 @@ public class DeviceConfiguration {
     public Topic getRootCAFilePath() {
         return kernel.getConfig().lookup(SYSTEM_NAMESPACE_KEY, DEVICE_PARAM_ROOT_CA_PATH).dflt("")
                 .addValidator(deTildeValidator);
+    }
+
+    public Topic getInterpolateComponentConfiguration() {
+        return getTopic(DEVICE_PARAM_INTERPOLATE_COMPONENT_CONFIGURATION).dflt(false);
     }
 
     public Topic getIotDataEndpoint() {
