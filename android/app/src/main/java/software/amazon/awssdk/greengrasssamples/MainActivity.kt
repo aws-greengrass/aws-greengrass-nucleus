81b189f3a19fcf8c0354b8f68fb53c386308d34a
sstanislavsky <78465460+sstanislavsky@users.noreply.github.com>
2021-12-27 17:54:13 +0100

    feat(android): ggsa-15 Create Android-specific folder and project structure (#4)
    * GGSA-15 Create Android-specific folder and project structure
    - created and setup a android project and greengrass sdk in the android folder
    - removed all change from root directories that related to android changes
    - roll back changes related to config

---
+++ android/app/src/main/java/software/amazon/awssdk/greengrasssamples/MainActivity.kt
@@ -0,0 +1,53 @@
+package software.amazon.awssdk.greengrasssamples
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.aws.greengrass.easysetup.GreengrassSetup
+import java.io.File
+
+class MainActivity : AppCompatActivity() {
+
+    private val nucleusParentThread = Thread {
+        Log.d(
+            "LOG_TAG",
+            "Nucleus parent thread started"
+        )
+        /** Perform periodic activities here  */
+        /** Perform periodic activities here  */
+        try {
+            // get applicationj writable folder
+            val dir = filesDir
+
+            // build greengrass v2 path and create it
+            val greengrass = File(dir, "greengrass")
+            val greengrassV2 = File(greengrass, "v2")
+            greengrassV2.mkdirs()
+
+            // set required properties
+            System.setProperty("log.store", "FILE")
+            System.setProperty("root", greengrassV2.absolutePath)
+            val fakeArgs =
+                arrayOf("--setup-system-service", "false")
+            GreengrassSetup.main(fakeArgs)
+
+            /* FIXME: Implement right way */while (true) {
+                Thread.sleep((30 * 1000).toLong())
+            }
+/*
+            Date now = Calendar.getInstance().getTime();
+            SimpleDateFormat df = new SimpleDateFormat("HH:mm:ss yyyy-MM-dd");
+            String formattedDate = df.format(now);
+            Log.d(LOG_TAG, "Nucleus parent thread is terminated. Timestamp: " + formattedDate);
+*/
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        nucleusParentThread.start()
+    }
+}
