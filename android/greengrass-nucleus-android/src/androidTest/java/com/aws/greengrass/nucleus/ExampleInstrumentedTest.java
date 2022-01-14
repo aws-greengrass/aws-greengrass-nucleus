05d5aa6c3fe8f05bc60778fb6c8a97e326a9e476
Andrew Shirokov <andrewshirokov@klika-tech.com>
2022-01-14 15:42:36 +0100

    chore(android): rename MainActivity package and application id

--- android/greengrass-nucleus-android/src/androidTest/java/com/aws/greengrass/nucleus/ExampleInstrumentedTest.java
+++ android/greengrass-nucleus-android/src/androidTest/java/com/aws/greengrass/nucleus/ExampleInstrumentedTest.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-package software.amazon.awssdk.iotdevicesdk;
+package com.aws.greengrass.nucleus;
 
 import static org.junit.Assert.assertEquals;
 
@@ -26,6 +26,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("software.amazon.awssdk.greengrass_nucleus_android.test", appContext.getPackageName());
+        assertEquals("com.aws.greengrass.nucleus.test", appContext.getPackageName());
     }
 }
--- android/greengrass-nucleus-android/src/test/java/com/aws/greengrass/nucleus/ExampleUnitTest.java
+++ android/greengrass-nucleus-android/src/test/java/com/aws/greengrass/nucleus/ExampleUnitTest.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-package software.amazon.awssdk.iotdevicesdk;
+package com.aws.greengrass.nucleus;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
