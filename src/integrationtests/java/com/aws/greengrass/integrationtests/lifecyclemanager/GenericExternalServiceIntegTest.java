4c2e32064243e80008db23a453abeae11a3dcd6f
andrew-shirokov <andrewshirokov@klika-tech.com>
2022-03-04 15:40:06 +0300

    feat(android): enable ci on test branch Increase timeouts

--- src/integrationtests/java/com/aws/greengrass/integrationtests/lifecyclemanager/GenericExternalServiceIntegTest.java
+++ src/integrationtests/java/com/aws/greengrass/integrationtests/lifecyclemanager/GenericExternalServiceIntegTest.java
@@ -335,7 +335,7 @@ class GenericExternalServiceIntegTest extends BaseITCase {
         service.getServiceConfig().find(SERVICE_LIFECYCLE_NAMESPACE_TOPIC, "install")
                 .withValue("echo \"Reinstalling service_with_dynamic_config\"");
 
-        assertTrue(serviceReinstalled.await(5, TimeUnit.SECONDS));
+        assertTrue(serviceReinstalled.await(30, TimeUnit.SECONDS));
     }
 
     @Test
@@ -365,7 +365,7 @@ class GenericExternalServiceIntegTest extends BaseITCase {
         });
         service.getServiceConfig().find(VERSION_CONFIG_KEY).withValue("1.0.1");
 
-        assertTrue(serviceReinstalled.await(5, TimeUnit.SECONDS));
+        assertTrue(serviceReinstalled.await(60, TimeUnit.SECONDS));
     }
 
     @Test
@@ -394,7 +394,7 @@ class GenericExternalServiceIntegTest extends BaseITCase {
         service.getServiceConfig().find(SERVICE_LIFECYCLE_NAMESPACE_TOPIC, "run")
                 .withValue("echo \"Rerunning service_with_dynamic_config\" && sleep 100");
 
-        assertTrue(serviceRestarted.await(5, TimeUnit.SECONDS));
+        assertTrue(serviceRestarted.await(30, TimeUnit.SECONDS));
     }
 
     @Test
