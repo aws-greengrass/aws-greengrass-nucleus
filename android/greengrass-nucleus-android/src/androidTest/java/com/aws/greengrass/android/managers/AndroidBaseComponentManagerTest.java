19d5c2ce27b73976675fbb38e6fc69d760caa9df
sstanislavsky <sstanislavsky@klika-tech.com>
2022-03-18 10:42:50 +0100

    feat(android): added header

--- android/greengrass-nucleus-android/src/androidTest/java/com/aws/greengrass/android/managers/AndroidBaseComponentManagerTest.java
+++ android/greengrass-nucleus-android/src/androidTest/java/com/aws/greengrass/android/managers/AndroidBaseComponentManagerTest.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 package com.aws.greengrass.android.managers;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
