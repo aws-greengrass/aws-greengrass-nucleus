64f901992f9276dd5ac9c1df69b8a60a38e2bd41
Saranya <nukai@amazon.com>
2022-04-19 15:17:54 -0700

    feat: add IPC operation for subscribing to cert updates (#1204)

---
+++ src/main/java/software/amazon/awssdk/aws/greengrass/GeneratedAbstractSubscribeToCertificateUpdatesOperationHandler.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package software.amazon.awssdk.aws.greengrass;
+
+import java.lang.Override;
+import software.amazon.awssdk.aws.greengrass.model.CertificateUpdateEvent;
+import software.amazon.awssdk.aws.greengrass.model.SubscribeToCertificateUpdatesRequest;
+import software.amazon.awssdk.aws.greengrass.model.SubscribeToCertificateUpdatesResponse;
+import software.amazon.awssdk.eventstreamrpc.OperationContinuationHandler;
+import software.amazon.awssdk.eventstreamrpc.OperationContinuationHandlerContext;
+import software.amazon.awssdk.eventstreamrpc.OperationModelContext;
+import software.amazon.awssdk.eventstreamrpc.model.EventStreamJsonMessage;
+
+public abstract class GeneratedAbstractSubscribeToCertificateUpdatesOperationHandler extends OperationContinuationHandler<SubscribeToCertificateUpdatesRequest, SubscribeToCertificateUpdatesResponse, EventStreamJsonMessage, CertificateUpdateEvent> {
+    protected GeneratedAbstractSubscribeToCertificateUpdatesOperationHandler(
+            OperationContinuationHandlerContext context) {
+        super(context);
+    }
+
+    @Override
+    public OperationModelContext<SubscribeToCertificateUpdatesRequest, SubscribeToCertificateUpdatesResponse, EventStreamJsonMessage, CertificateUpdateEvent> getOperationModelContext(
+    ) {
+        return GreengrassCoreIPCServiceModel.getSubscribeToCertificateUpdatesModelContext();
+    }
+}
