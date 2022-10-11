843d894d44435699cff2844ebd7d668f524a0015
Joseph Cosentino <joecose@amazon.com>
2022-10-11 16:52:18 -0700

    feat(tes): support ipv6

--- src/main/java/com/aws/greengrass/tes/HttpServerImpl.java
+++ src/main/java/com/aws/greengrass/tes/HttpServerImpl.java
@@ -11,17 +11,14 @@ import com.sun.net.httpserver.HttpServer;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.util.concurrent.ExecutorService;
-import javax.inject.Inject;
 
 public class HttpServerImpl implements Server {
     public static final String URL = "/2016-11-01/credentialprovider/";
-    private static final int TIME_TO_WAIT_BEFORE_SHUTDOWN_IN_SECONDS = 1;
-    private final HttpServer httpImpl;
-
-    @Inject
+    private static final int STOP_TIMEOUT_SECONDS = 1;
+    private static final String ANY_INTERFACE = "::";
+    private HttpServer server;
+    private final int configuredPort;
     private final HttpHandler credentialRequestHandler;
-
-    @Inject
     private final ExecutorService executorService;
 
     /**
@@ -31,25 +28,27 @@ public class HttpServerImpl implements Server {
      * @param executorService executor service instance
      * @throws IOException When server creation fails
      */
-    HttpServerImpl(int port, HttpHandler credentialRequestHandler, ExecutorService executorService) throws IOException {
-        httpImpl = HttpServer.create(new InetSocketAddress("localhost", port), 0);
+    HttpServerImpl(int port, HttpHandler credentialRequestHandler, ExecutorService executorService)
+            throws IOException {
+        this.configuredPort = port;
         this.credentialRequestHandler = credentialRequestHandler;
         this.executorService = executorService;
     }
 
     @Override
-    public void start() {
-        httpImpl.createContext(URL, credentialRequestHandler);
-        httpImpl.setExecutor(executorService);
-        httpImpl.start();
+    public void start() throws IOException {
+        server = HttpServer.create(new InetSocketAddress(ANY_INTERFACE, configuredPort), 0);
+        server.createContext(URL, credentialRequestHandler);
+        server.setExecutor(executorService);
+        server.start();
     }
 
     @Override
     public void stop() {
-        httpImpl.stop(TIME_TO_WAIT_BEFORE_SHUTDOWN_IN_SECONDS);
+        server.stop(STOP_TIMEOUT_SECONDS);
     }
 
     int getServerPort() {
-        return httpImpl.getAddress().getPort();
+        return server.getAddress().getPort();
     }
 }
--- src/test/java/com/aws/greengrass/tes/HttpServerImplTest.java
+++ src/test/java/com/aws/greengrass/tes/HttpServerImplTest.java
@@ -6,90 +6,102 @@
 package com.aws.greengrass.tes;
 
 import com.aws.greengrass.testcommons.testutilities.GGExtension;
-import com.sun.net.httpserver.HttpExchange;
+import com.aws.greengrass.testcommons.testutilities.TestUtils;
 import com.sun.net.httpserver.HttpHandler;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
 import java.net.URL;
+import java.util.Enumeration;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
-import static org.mockito.Mockito.any;
-import static org.mockito.Mockito.doAnswer;
 
 @ExtendWith({MockitoExtension.class, GGExtension.class})
 class HttpServerImplTest {
-    private static final String mockResponse = "Hello World";
 
-    private final ExecutorService executorService = Executors.newFixedThreadPool(1);
+    private static final String MOCK_CREDENTIAL_RESPONSE = "Hello World";
 
-    @Mock
-    private HttpHandler mockHttpHandler;
+    ExecutorService executorService = TestUtils.synchronousExecutorService();
 
-    private HttpServerImpl startServer(int port) {
-        HttpServerImpl server = null;
-        try {
-            server = new HttpServerImpl(port, mockHttpHandler, executorService);
-            server.start();
-        } catch (IOException e) {
-            fail("Could not start the server: {}", e);
+    HttpHandler mockCredentialRequestHandler = exchange -> {
+        exchange.sendResponseHeaders(HttpURLConnection.HTTP_OK, MOCK_CREDENTIAL_RESPONSE.length());
+        exchange.getResponseBody().write(MOCK_CREDENTIAL_RESPONSE.getBytes());
+        exchange.close();
+    };
+
+    HttpServerImpl server;
+
+    int withServer(int port) throws IOException {
+        server = new HttpServerImpl(port, mockCredentialRequestHandler, executorService);
+        server.start();
+        return server.getServerPort();
+    }
+
+    @AfterEach
+    void tearDown() {
+        if (server != null) {
+            server.stop();
         }
-        return server;
     }
 
-    private void stopServer(HttpServerImpl server) {
-        server.stop();
-        executorService.shutdown();
-    }
-
-    @SuppressWarnings("PMD.CloseResource")
     @ParameterizedTest
     @ValueSource(ints = {0, 1025, 65355})
     void GIVEN_port_WHEN_server_started_THEN_requests_are_successful(int port) throws Exception {
-        HttpServerImpl server = startServer(port);
-        try {
-            doAnswer(invocationArgs -> {
-                HttpExchange args = (HttpExchange) invocationArgs.getArguments()[0];
-                args.sendResponseHeaders(HttpURLConnection.HTTP_OK, mockResponse.length());
-                args.getResponseBody().write(mockResponse.getBytes());
-                args.close();
-                return null; //void method
-            }).when(mockHttpHandler).handle(any());
-            if (port == 0) {
-                port = server.getServerPort();
-            }
-            URL url = new URL("http://localhost:" + port + HttpServerImpl.URL);
-            HttpURLConnection con = (HttpURLConnection) url.openConnection();
-            con.setRequestMethod("GET");
-            con.setDoOutput(true);
-            OutputStream out = con.getOutputStream();
-            out.flush();
-            out.close();
+        Set<String> hostAddresses = getLoopbackAddresses().stream()
+                .map(InetAddress::getHostAddress)
+                .collect(Collectors.toSet());
 
-            InputStream ip = con.getInputStream();
-            BufferedReader br = new BufferedReader(new InputStreamReader(ip));
+        int resolvedPort = withServer(port);
 
-            StringBuilder response = new StringBuilder();
-            String responseSingle = br.readLine();
-            while (responseSingle != null) {
-                response.append(responseSingle);
-                responseSingle = br.readLine();
-            }
-            assertEquals(mockResponse, response.toString());
-        } finally {
-            stopServer(server);
+        for (String host : hostAddresses) {
+            String tesResponse = sendTESRequest(host, resolvedPort);
+            assertEquals(MOCK_CREDENTIAL_RESPONSE, tesResponse);
         }
+    }
+
+    private static String sendTESRequest(String host, int port) throws IOException {
+        URL url = new URL("http", host, port, HttpServerImpl.URL);
+
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("GET");
+        con.setDoOutput(true);
+
+        try (OutputStream out = con.getOutputStream()) {
+            out.flush();
+        }
+
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream()))) {
+            return br.lines().collect(Collectors.joining("\n"));
+        }
+    }
+
+    private static Set<InetAddress> getLoopbackAddresses() throws IOException {
+        Set<InetAddress> loopbackAddresses = new HashSet<>();
+        Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
+        while (interfaces.hasMoreElements()) {
+            NetworkInterface nif = interfaces.nextElement();
+            if (!nif.isLoopback()) {
+                continue;
+            }
+            Enumeration<InetAddress> addresses = nif.getInetAddresses();
+            while (addresses.hasMoreElements()) {
+                loopbackAddresses.add(addresses.nextElement());
+            }
+        }
+        return loopbackAddresses;
     }
 }
