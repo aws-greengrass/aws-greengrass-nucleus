cfe9b893f268d159aebaf5a0c55bed8a78a029e4
jing.jiang <jing.jiang@thundersoft.com>
2022-10-18 22:39:29 +0800

    fix: createUser, createGroup and addUserToGroup do not support Busybox

--- src/test/java/com/aws/greengrass/util/platforms/unix/UnixPlatformTest.java
+++ src/test/java/com/aws/greengrass/util/platforms/unix/UnixPlatformTest.java
@@ -5,100 +5,28 @@
 
 package com.aws.greengrass.util.platforms.unix;
 
-import com.aws.greengrass.logging.api.Logger;
-import com.aws.greengrass.logging.impl.LogManager;
 import com.aws.greengrass.testcommons.testutilities.GGExtension;
 import com.aws.greengrass.util.FileSystemPermission;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.DisabledOnOs;
 import org.junit.jupiter.api.condition.OS;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Spy;
-import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.io.IOException;
 import java.nio.file.attribute.PosixFilePermission;
-import java.util.Arrays;
 import java.util.Set;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.arrayContaining;
 import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.containsInAnyOrder;
-import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.empty;
 import static org.hamcrest.Matchers.is;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
-
-@ExtendWith({MockitoExtension.class, GGExtension.class})
+@ExtendWith({GGExtension.class})
 @DisabledOnOs(OS.WINDOWS)
 class UnixPlatformTest   {
 
     private static String[] command = {"echo", "hello", "world"};
-    private static final Logger logger = LogManager.getLogger(UnixPlatformTest.class);
-
-    @Spy
-    private final UnixPlatform spyPlatform = new UnixPlatform();
-    private final static String user = "ggc_user01";
-    private final static String group = "ggc_group01";
-
-    @Test
-    public void GIVEN_busybox_environment_WHEN_add_user_THEN_add_user_success() {
-        try {
-            spyPlatform.runCmd("useradd -r -m " + user, o -> {
-            }, "Failed to create user with useradd");
-        } catch (IOException e) {
-            logger.info(e.getMessage());
-        }
-
-        try {
-            spyPlatform.createUser(user);
-        } catch (IOException e) {
-            Throwable throwable = assertThrows(IOException.class, () -> spyPlatform.createUser(user));
-            Throwable[] suppressed = throwable.getSuppressed();
-            assertThat(throwable.getMessage(), containsString("useradd"));
-            assertThat(String.valueOf(Arrays.stream(suppressed).anyMatch(item -> item.toString().contains("adduser"))), true);
-        }
-    }
-
-    @Test
-    public void GIVEN_busybox_environment_WHEN_add_group_THEN_add_group_success() {
-        try {
-            spyPlatform.runCmd("groupadd -r " + group, o -> {
-            }, "Failed to create group with groupadd");
-        } catch (IOException e) {
-            logger.info(e.getMessage());
-        }
-
-        try {
-            spyPlatform.createGroup(group);
-        } catch (IOException e) {
-            Throwable throwable = assertThrows(IOException.class, () -> spyPlatform.createGroup(group));
-            Throwable[] suppressed = throwable.getSuppressed();
-            assertThat(String.valueOf(Arrays.stream(suppressed).anyMatch(item -> item.toString().contains("addgroup"))), true);
-            assertThat(throwable.getMessage(), containsString("groupadd"));
-        }
-    }
-
-    @Test
-    public void GIVEN_not_unix_platform_WHEN_add_user_to_group_THEN_user_added_to_group_successfully() {
-        try {
-            spyPlatform.runCmd("groupdel " + group, o -> {
-            }, "Failed to del group with groupdel");
-        } catch (IOException e) {
-            logger.info(e.getMessage());
-        }
-
-        try {
-            spyPlatform.addUserToGroup(user, group);
-        } catch (IOException e) {
-            Throwable throwable = assertThrows(IOException.class, () -> spyPlatform.addUserToGroup(user, group));
-            Throwable[] suppressed = throwable.getSuppressed();
-            assertThat(throwable.getMessage(), containsString("usermod"));
-            assertThat(String.valueOf(Arrays.stream(suppressed).anyMatch(item -> item.toString().contains("addgroup ggc_user01 ggc_group01"))), true);
-        }
-    }
 
     @Test
     void GIVEN_no_user_and_no_group_WHEN_decorate_THEN_do_not_generate_sudo_with_user_and_group() {
