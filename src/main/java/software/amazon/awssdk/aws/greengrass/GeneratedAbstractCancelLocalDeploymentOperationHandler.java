a33c8662b2cc3d02f89459a5701d1acd1a4adb41
Junfu Chen <chenjunf@amazon.com>
2023-05-09 17:05:05 -0700

    feat: allow local deployment cancellation

---
+++ src/main/java/software/amazon/awssdk/aws/greengrass/GeneratedAbstractCancelLocalDeploymentOperationHandler.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package software.amazon.awssdk.aws.greengrass;
+
+import software.amazon.awssdk.aws.greengrass.model.CancelLocalDeploymentRequest;
+import software.amazon.awssdk.aws.greengrass.model.CancelLocalDeploymentResponse;
+import software.amazon.awssdk.eventstreamrpc.OperationContinuationHandler;
+import software.amazon.awssdk.eventstreamrpc.OperationContinuationHandlerContext;
+import software.amazon.awssdk.eventstreamrpc.OperationModelContext;
+import software.amazon.awssdk.eventstreamrpc.model.EventStreamJsonMessage;
+
+public abstract class GeneratedAbstractCancelLocalDeploymentOperationHandler extends
+        OperationContinuationHandler<CancelLocalDeploymentRequest, CancelLocalDeploymentResponse,
+                EventStreamJsonMessage, EventStreamJsonMessage> {
+    protected GeneratedAbstractCancelLocalDeploymentOperationHandler(OperationContinuationHandlerContext context) {
+        super(context);
+    }
+
+    @Override
+    public OperationModelContext<CancelLocalDeploymentRequest, CancelLocalDeploymentResponse, EventStreamJsonMessage,
+            EventStreamJsonMessage> getOperationModelContext() {
+        return GreengrassCoreIPCServiceModel.getCancelLocalDeploymentModelContext();
+    }
+}
