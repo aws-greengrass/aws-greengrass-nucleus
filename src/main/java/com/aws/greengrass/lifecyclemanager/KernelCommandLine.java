66eb0d8dbdc29b215f66692fbc9b0f6f230657a7
Kody Stribrny <kstribrn@amazon.com>
2025-04-22 09:26:52 -0700

    chore: add user name character warning
    Adds a warning log when potentially
    invalid characters are used in a username.

--- src/main/java/com/aws/greengrass/lifecyclemanager/KernelCommandLine.java
+++ src/main/java/com/aws/greengrass/lifecyclemanager/KernelCommandLine.java
@@ -64,6 +64,7 @@ public class KernelCommandLine {
     private static final String deploymentsPathName = "~root/deployments";
     private static final String cliIpcInfoPathName = "~root/cli_ipc_info";
     private static final String binPathName = "~root/bin";
+    private static final String validUserCharExpr = "^[a-zA-Z0-9._]+[a-zA-Z0-9._-]*$";
 
     public KernelCommandLine(Kernel kernel) {
         this(kernel, kernel.getNucleusPaths());
@@ -158,6 +159,10 @@ public class KernelCommandLine {
         }
         if (defaultUserFromCmdLine != null) {
             if (PlatformResolver.isWindows) {
+                if (!defaultUserFromCmdLine.matches(validUserCharExpr)) {
+                    logger.warn("Component user may contain invalid characters. This can cause issues starting a "
+                            + "component.");
+                }
                 deviceConfiguration.getRunWithDefaultWindowsUser().withValue(defaultUserFromCmdLine);
             } else {
                 deviceConfiguration.getRunWithDefaultPosixUser().withValue(defaultUserFromCmdLine);
